8726c3ecc348cd18f483e117c6d6002c3d1e9153
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-26 03:33:53 +0800

    Update Rock index: add "Nirvana - Smell Like Teen Spirit [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Gorillaz - Feel Good Inc [MIDIfind.Com]",
+	"Nirvana - Smell Like Teen Spirit [MIDIfind.Com]",
 	"Soda Stereo - De Musica Ligera [MIDIfind.Com]",
 }
