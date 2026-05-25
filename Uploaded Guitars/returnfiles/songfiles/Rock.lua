ba5c4fbc658983a3eee95bd39531a55e1589e611
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-26 04:26:33 +0800

    Update Rock index: add "ACDC - Thunderstruck [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"ACDC - Thunderstruck [MIDIfind.Com]",
 	"Gorillaz - Feel Good Inc [MIDIfind.Com]",
 	"Metallica - Master Of Puppets (22) [MIDIfind.Com]",
 	"Nirvana - Smell Like Teen Spirit [MIDIfind.Com]",
