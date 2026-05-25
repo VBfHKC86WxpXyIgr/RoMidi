ca24a786a650c1669c4b9cf37be622d160cf2f69
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-26 01:14:42 +0800

    Update GameOST index: add "Penumbra Phantasm LWP" [piano] via uploader

--- UploadedMidi/uploadreturn/GameOSTreturn.lua
+++ UploadedMidi/uploadreturn/GameOSTreturn.lua
@@ -5,5 +5,6 @@ return {
 	"Descend [PianoSolo]",
 	"MeGaLoVania Piano",
 	"Oppa Toby Style - Homestuck S Collide OST 2 For Piano RWE",
+	"Penumbra Phantasm LWP",
 	"Rex Duodecim Angelus Homestuck 1776449720878",
 }
