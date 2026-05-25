a3f4ef0275d9ae086e5a6f8e1f82abd82fe9f0f2
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-26 03:34:12 +0800

    Update Rock index: add "Metallica - Master Of Puppets (22) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Gorillaz - Feel Good Inc [MIDIfind.Com]",
+	"Metallica - Master Of Puppets (22) [MIDIfind.Com]",
 	"Nirvana - Smell Like Teen Spirit [MIDIfind.Com]",
 	"Soda Stereo - De Musica Ligera [MIDIfind.Com]",
 }
