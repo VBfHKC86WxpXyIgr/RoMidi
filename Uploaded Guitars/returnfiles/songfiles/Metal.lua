59e7e72bace1bf180b405a50a9483dd41adf627a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-27 04:25:00 +0800

    Update Metal index: add "Avenged Sevenfold - This Means War (Guitar Solo) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -1,4 +1,5 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Avenged Sevenfold - Hail To The King Solo [MIDIfind.Com]",
+	"Avenged Sevenfold - This Means War (Guitar Solo) [MIDIfind.Com]",
 }
