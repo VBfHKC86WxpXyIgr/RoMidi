70ac70b66ad561f1414f91a915a676cf73cea314
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-27 04:25:20 +0800

    Update Metal index: add "Avenged Sevenfold - Nightmare Solo Per By David Escobar [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -2,6 +2,7 @@
 return {
 	"Avenged Sevenfold - Dancing Dead Solo [MIDIfind.Com]",
 	"Avenged Sevenfold - Hail To The King Solo [MIDIfind.Com]",
+	"Avenged Sevenfold - Nightmare Solo Per By David Escobar [MIDIfind.Com]",
 	"Avenged Sevenfold - Save Me (2) [MIDIfind.Com]",
 	"Avenged Sevenfold - This Means War (Guitar Solo) [MIDIfind.Com]",
 }
