c373c245b49c475e4e514049195ade1cf52b9a4f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-27 04:25:07 +0800

    Update Metal index: add "Avenged Sevenfold - Dancing Dead Solo [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Avenged Sevenfold - Dancing Dead Solo [MIDIfind.Com]",
 	"Avenged Sevenfold - Hail To The King Solo [MIDIfind.Com]",
 	"Avenged Sevenfold - This Means War (Guitar Solo) [MIDIfind.Com]",
 }
