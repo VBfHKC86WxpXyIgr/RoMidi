88d77beff3b664011e66f1e03fbb6ecca1d60c28
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-27 21:18:46 +0800

    Update Acoustic index: add "Bleach - Ranbu No Melody (OP 13)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Bleach - Ranbu No Melody (OP 13)",
 	"Can'T Help Falling In Love - Elvis Presley",
 	"Radiohead - No Surprises",
 }
