96ac90bdf2b75fe71a95ee5273f958409fb60a8e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-28 05:51:34 +0800

    Update OST index: add "The Legend Of Zelda Great Fairy Fountain (Piano Cover)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/OST.lua
+++ Uploaded Guitars/returnfiles/songfiles/OST.lua
@@ -3,4 +3,5 @@ return {
 	"Dire, Dire Docks (Super Mario 64)",
 	"Grand Theft Auto San Andreas - Intro [MIDIfind.Com]",
 	"OFFICIAL Roblox Bakon Theme Song",
+	"The Legend Of Zelda Great Fairy Fountain (Piano Cover)",
 }
