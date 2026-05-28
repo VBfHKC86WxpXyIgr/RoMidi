0147804fd0f8abd959cbd5ccc2172ce10ad858d3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-29 01:51:01 +0800

    Update Acoustic index: add "Ed Sheeran Thinking Out Loud" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -3,5 +3,6 @@ return {
 	"Bleach - Ranbu No Melody (OP 13)",
 	"Can'T Help Falling In Love - Elvis Presley",
 	"Come As You Are - Nirvana (Guitar Tab)",
+	"Ed Sheeran Thinking Out Loud",
 	"Radiohead - No Surprises",
 }
