fc96f6d454d9eda486978b1d46fb6a3440b2553b
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-29 12:07:38 +0800

    Update Others index: add "RUSH C ULTRA IMPOSSIBLE" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -4,6 +4,7 @@ return {
 	"Eldoraudio-Flower Dance - DJ Okawari Very Hard Ver. Piano Arranging By. Lune Music Sheets MP3 320K",
 	"Little Do U Know",
 	"Pixies - Where Is My Mind [MIDIfind.Com]",
+	"RUSH C ULTRA IMPOSSIBLE",
 	"System Of A Down - Aerials (18) [MIDIfind.Com]",
 	"The Nuker 4",
 	"The Nuker 4 Part 1",
