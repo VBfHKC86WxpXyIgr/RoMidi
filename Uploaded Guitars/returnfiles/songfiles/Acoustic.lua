a5aa7f21d0e06c672ff49957b41d001d4ebf5f40
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-30 01:56:01 +0800

    Update Acoustic index: add "Chris Isaakwicked Game" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -3,6 +3,7 @@ return {
 	"Andra And The BackBone - Sempurna Lyre.Mid",
 	"Bleach - Ranbu No Melody (OP 13)",
 	"Can'T Help Falling In Love - Elvis Presley",
+	"Chris Isaakwicked Game",
 	"Come As You Are - Nirvana (Guitar Tab)",
 	"Ed Sheeran Thinking Out Loud",
 	"Radiohead - No Surprises",
