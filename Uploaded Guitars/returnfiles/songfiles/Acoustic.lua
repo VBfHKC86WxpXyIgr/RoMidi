f50c752845fa336ee1961b5079a1b9df87efdcc4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-05-29 22:53:33 +0800

    Update Acoustic index: add "Andra And The BackBone - Sempurna Lyre.Mid" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Andra And The BackBone - Sempurna Lyre.Mid",
 	"Bleach - Ranbu No Melody (OP 13)",
 	"Can'T Help Falling In Love - Elvis Presley",
 	"Come As You Are - Nirvana (Guitar Tab)",
