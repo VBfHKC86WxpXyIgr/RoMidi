c7562dbf92d63de4cd136e12536fdab49e7f5891
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-02 07:02:37 +0800

    Update Modern index: add "Pasilyo - Sun Kissed Lola (Piano Cover)" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -48,6 +48,7 @@ return {
 	"Night Changes Piano Arr 1775503451731",
 	"No batidão impossible",
 	"Nơi này có anh impossible",
+	"Pasilyo - Sun Kissed Lola (Piano Cover)",
 	"rapp snitch knishes",
 	"Rapp Snitch Knishes - MF DOOM",
 	"Rush E – lyre only",
