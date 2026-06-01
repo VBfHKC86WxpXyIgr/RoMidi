ebd852f61616c8fb8b7ed2ad43329e9c782af57f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-01 08:36:42 +0800

    Update Classical index: add "Bogemian" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -12,6 +12,7 @@ return {
 	"Beethoven Virus (insane)",
 	"Beethoven Virus (insane).mid (1)",
 	"Binks Rum Binks Sake 1780241727588",
+	"Bogemian",
 	"Bruno Mars - Risk It All",
 	"Chopin – Nocturne Op.9 No.2",
 	"chopin ballade 4 (c)lubetsky",
