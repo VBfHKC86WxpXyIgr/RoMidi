a69052136c9f5f717ac315293daa1a5b84b44be4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-02 10:44:00 +0800

    Update Modern index: add "Talking To The Moon (Bruno Mars).Mid" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -67,6 +67,7 @@ return {
 	"Someone Like You (Easy Piano)",
 	"Star Shopping Midi.Mid",
 	"Strawberry Guy - Mrs Magic (Strings Version)",
+	"Talking To The Moon (Bruno Mars).Mid",
 	"Tally Hall- Hidden In The Sand",
 	"Tek It - Cafune",
 	"The+New+Periodic+Table+Song",
