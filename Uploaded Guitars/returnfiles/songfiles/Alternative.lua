4a5cf91954664321525e55f7be5ba57060607479
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 18:53:27 +0800

    Update Alternative index: add "Creep - Radiohead - Extremely Accurate (Full Band) G" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Alternative.lua
+++ Uploaded Guitars/returnfiles/songfiles/Alternative.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Chase Batta - JoJo'S Bizarre Adventure Part4 Diamond Is Unbreakable OP2",
+	"Creep - Radiohead - Extremely Accurate (Full Band) G",
 	"Everlong By The Foo Fighters Piia",
 	"In The End - Linkin Park",
 	"Jane! Ttest",
