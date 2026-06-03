ac494441bf45b4edd22cff9522c82d59b78f63be
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 19:19:56 +0800

    Update Classic Rock index: add "Through The Fire And Flames - DragonForce (Complete, Virtuoso Version)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Megalovania On Guitar",
+	"Through The Fire And Flames - DragonForce (Complete, Virtuoso Version)",
 	"Wake Me Up When September Ends - Green Day (Guitar Tab Play-Along)",
 }
