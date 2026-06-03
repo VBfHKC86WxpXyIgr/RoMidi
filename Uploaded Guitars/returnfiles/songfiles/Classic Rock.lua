84c099d93a10e94e5e137264d2b3d8f9515f5fd6
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 20:40:10 +0800

    Update Classic Rock index: add "Shiki No Uta (1)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -10,6 +10,7 @@ return {
 	"Megalovania On Guitar",
 	"PAUSE",
 	"Playing God - Polyphia Playing God Tab",
+	"Shiki No Uta (1)",
 	"Through The Fire And Flames - DragonForce (Complete, Virtuoso Version)",
 	"Tuyo (Narcos Theme) - Rodrigo Amarante",
 	"Wake Me Up When September Ends - Green Day (Guitar Tab Play-Along)",
