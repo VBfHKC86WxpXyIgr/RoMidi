c1897fef6448d594cfe4910c815199bca66bb996
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 20:21:41 +0800

    Update Classic Rock index: add "Hava Nagila A Z Idelsohn" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -3,6 +3,7 @@ return {
 	"Buddy Holly - Weezer (Extremely Accurate) 🔥🎸G",
 	"Cant Stop - Red Hot Chili Peppers",
 	"Guns N Roses - Sweet Child O Mine",
+	"Hava Nagila A Z Idelsohn",
 	"Megalovania On Guitar",
 	"Through The Fire And Flames - DragonForce (Complete, Virtuoso Version)",
 	"Wake Me Up When September Ends - Green Day (Guitar Tab Play-Along)",
