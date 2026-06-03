5f4637d49b53c84c208d39c61e1d7a5a9986596c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 18:53:12 +0800

    Update Alternative index: add "Chase Batta - JoJo'S Bizarre Adventure Part4 Diamond Is Unbreakable OP2" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Alternative.lua
+++ Uploaded Guitars/returnfiles/songfiles/Alternative.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Chase Batta - JoJo'S Bizarre Adventure Part4 Diamond Is Unbreakable OP2",
 	"Everlong By The Foo Fighters Piia",
 	"In The End - Linkin Park",
 	"Jane! Ttest",
