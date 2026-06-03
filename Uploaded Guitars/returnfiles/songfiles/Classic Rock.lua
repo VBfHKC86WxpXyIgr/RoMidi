5772292e50a8b2db72e7d29f8cb00cf93cea40ee
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 20:27:35 +0800

    Update Classic Rock index: add "Tuyo (Narcos Theme) - Rodrigo Amarante" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -7,6 +7,7 @@ return {
 	"Hava Nagila A Z Idelsohn",
 	"Megalovania On Guitar",
 	"Through The Fire And Flames - DragonForce (Complete, Virtuoso Version)",
+	"Tuyo (Narcos Theme) - Rodrigo Amarante",
 	"Wake Me Up When September Ends - Green Day (Guitar Tab Play-Along)",
 	"แสงสุดท้าย (Saeng Sud Tye) - Bodyslam [Fingerstyle]",
 }
