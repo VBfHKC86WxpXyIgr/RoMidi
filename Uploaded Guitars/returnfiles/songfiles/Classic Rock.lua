f9fb9f55ca3b958f916bbee44b1724e08edee5b5
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-03 19:35:28 +0800

    Update Classic Rock index: add "Cant Stop - Red Hot Chili Peppers" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Buddy Holly - Weezer (Extremely Accurate) 🔥🎸G",
+	"Cant Stop - Red Hot Chili Peppers",
 	"Megalovania On Guitar",
 	"Through The Fire And Flames - DragonForce (Complete, Virtuoso Version)",
 	"Wake Me Up When September Ends - Green Day (Guitar Tab Play-Along)",
