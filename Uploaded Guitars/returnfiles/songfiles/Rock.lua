6812ee1543ebd54504b8ed2be2dd231ce118f844
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-04 17:46:07 +0800

    Update Rock index: add "Romantic Homicide" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -17,6 +17,7 @@ return {
 	"Nirvana - Smell Like Teen Spirit [MIDIfind.Com]",
 	"Nirvana - Smell Like Teen Spirit [MIDIfind.Com] (1)",
 	"Rachmaninoff - Piano Concerto No. 2 Op. 18 - 2nd Mvmt - Piano Solo",
+	"Romantic Homicide",
 	"Soda Stereo - De Musica Ligera [MIDIfind.Com]",
 	"System Of A Down - Toxicity [MIDIfind.Com]",
 }
