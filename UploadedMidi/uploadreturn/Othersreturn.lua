9760d081fbdce708c118d4d5e30c5c94cf42b664
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-06 18:51:16 +0800

    Update Others index: add "Rush E (Impossible).Transformed" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -6,6 +6,7 @@ return {
 	"(Anybody Can Find Love Except You) Need Drums Mhxa",
 	"[Black MIDI] RUSH E CURSED REMIX",
 	"Feather - Nujabes",
+	"Rush E (Impossible).Transformed",
 	"Rush E 4",
 	"Rush E 4 No Lag",
 }
