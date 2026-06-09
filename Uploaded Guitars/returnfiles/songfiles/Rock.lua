f405e4df1905bad4823d98834f0252e016234524
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-09 18:48:10 +0800

    Update Rock index: add "Creed - One Last Breath [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -11,6 +11,7 @@ return {
 	"Come As You Are (Unplugged) - Nirvana Guitar",
 	"Come As You Are Nirvana Bass",
 	"Come As You Are Nirvana Guitar",
+	"Creed - One Last Breath [MIDIfind.Com]",
 	"Dont Say Lazy",
 	"Free Bird - Lynyrd Skynyrd",
 	"Gorillaz - Feel Good Inc [MIDIfind.Com]",
