2ed4df1c65155e1f074ec3cdfb4ef5dc541743dd
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-10 10:13:50 +0800

    Update Pop index: add "Chamber Of Reflection - Mac DeMarco-Drum Kit, Set De Batterie" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Pop.lua
+++ Uploaded Drums/returnfiles/songfiles/Pop.lua
@@ -1,5 +1,7 @@
+-- Auto-updated by the Cloudflare Worker on upload/delete
 return {
-    "Blinding Lights - The Weeknd",
-    "europop drumplosion",
-    "Mac DeMarco - Chamber of Reflection"
+	"Blinding Lights - The Weeknd",
+	"Chamber Of Reflection - Mac DeMarco-Drum Kit, Set De Batterie",
+	"europop drumplosion",
+	"Mac DeMarco - Chamber of Reflection",
 }
