206fdb9a5853dc27c80366ad42b8e1f4d994f4b9
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-11 07:11:50 +0800

    Update Rock index: add "Alice In Chains - Man In A Box" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -2,6 +2,7 @@
 return {
 	"21 Guns - Green Day (Piano-Vocal-Guitar)",
 	"ACDC - Thunderstruck [MIDIfind.Com]",
+	"Alice In Chains - Man In A Box",
 	"American Football - Never Meant",
 	"Bismarck - Sabaton Piano Arrangement",
 	"Black Rover - Black Clover (OP 3)",
