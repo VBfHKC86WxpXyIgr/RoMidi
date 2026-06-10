e9d77a46cebdc55ba626d2ff3345472e4ab454d6
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-10 13:20:45 +0800

    Update Alternative index: add "Bjork - Army Of Me WWW.MIDISFREE.COM" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Alternative.lua
+++ Uploaded Guitars/returnfiles/songfiles/Alternative.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Aerosmith - Dream On",
+	"Bjork - Army Of Me WWW.MIDISFREE.COM",
 	"Chase Batta - JoJo'S Bizarre Adventure Part4 Diamond Is Unbreakable OP2",
 	"Creep - Radiohead - Extremely Accurate (Full Band) G",
 	"Everlong By The Foo Fighters Piia",
