4ed07d699e07861ac220db3a88eb26c7796b61da
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-11 09:06:44 +0800

    Update Modern index: add "Michael Jackson - Bad" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -63,6 +63,7 @@ return {
 	"Love Story X Golden Brown",
 	"Love Story X Golden Brown (1)",
 	"lover girl - laufey",
+	"Michael Jackson - Bad",
 	"Michael Jackson - Don’T Stop Till You Get Enough",
 	"Michael Jackson - Smooth Criminal",
 	"Michael Jackson (They Don'T Care About Us) Complete",
