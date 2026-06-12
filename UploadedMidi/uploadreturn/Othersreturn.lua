d65291ca0dc0f9133da08d7d9c3f4564a4a6cd62
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-12 19:05:13 +0800

    Update Others index: add "TáI Sinh" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -103,6 +103,7 @@ return {
 	"Song For You",
 	"Sword Art Online Gun Gale",
 	"Sword Art Online II Opening 1 - IGNITE",
+	"TáI Sinh",
 	"Tay TráI Chỉ TrăNg",
 	"TháNg 4 Là LờI NóI DốI CủA Em",
 	"That Girl",
