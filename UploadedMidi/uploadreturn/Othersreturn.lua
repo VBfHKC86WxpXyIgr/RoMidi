e18def0422b3f8d3a9513e4b15f523a39ee6f628
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-12 19:14:27 +0800

    Update Others index: add "VếT MưA" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -149,6 +149,7 @@ return {
 	"Trả Cho Em Remix",
 	"TrướC Khi Em TồN TạI",
 	"Valentine Chờ",
+	"VếT MưA",
 	"ViếT MưA X đáY BiểN X Khoá BiệT Ly X ChịU CáCh MìNh NóI Thua X TháNg Tư Là LờI NóI DốI CủA Em X Waiting For You X LạC TrôI X NơI NàY Có Anh X DạO BướC Hong Kong X Peaches X Broken World",
 	"Virtual To Live",
 	"Virtual To LIVE Easy",
