30914557f9fcbfe6b6a7815a54ebc67e897a2114
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-13 22:03:05 +0800

    Update Punk index: add "Green Day - She'S A Rebel [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -6,5 +6,6 @@ return {
 	"Green Day - Give Me Novacaine [MIDIfind.Com]",
 	"Green Day - Holiday [MIDIfind.Com]",
 	"Green Day - Jesus Of Suburbia (2) [MIDIfind.Com]",
+	"Green Day - She'S A Rebel [MIDIfind.Com]",
 	"Green Dayst Jimmy",
 }
