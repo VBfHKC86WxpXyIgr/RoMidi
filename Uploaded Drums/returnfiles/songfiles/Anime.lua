da34af60b0674b1814336f533f69961f4907162d
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-13 20:21:39 +0800

    Update Anime index: add "Hkmori - Anybody Can Find Love" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Anime.lua
+++ Uploaded Drums/returnfiles/songfiles/Anime.lua
@@ -1,5 +1,7 @@
+-- Auto-updated by the Cloudflare Worker on upload/delete
 return {
-    "(WIP) Kessoku Band - Seishun Complex (Bocchi the Rock!)",
-    "81620 Guitar-Loneliness-and-Blue-Planet",
-    "Bocchi the Rock! Kessoku Band 結束バンド - Ano Band あのバンド"
+	"(WIP) Kessoku Band - Seishun Complex (Bocchi the Rock!)",
+	"81620 Guitar-Loneliness-and-Blue-Planet",
+	"Bocchi the Rock! Kessoku Band 結束バンド - Ano Band あのバンド",
+	"Hkmori - Anybody Can Find Love",
 }
