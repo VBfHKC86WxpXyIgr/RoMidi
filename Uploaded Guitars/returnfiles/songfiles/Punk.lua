695744e1b3e5ca3546c14787747aa4197e5efec9
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-13 22:01:48 +0800

    Update Punk index: add "Green Day - Give Me Novacaine [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -3,6 +3,7 @@ return {
 	"Green Day - American Idiot (2) [MIDIfind.Com]",
 	"Green Day - Are We The Waiting [MIDIfind.Com]",
 	"Green Day - Boulevard Of Broken Dreams [MIDIfind.Com]",
+	"Green Day - Give Me Novacaine [MIDIfind.Com]",
 	"Green Day - Holiday [MIDIfind.Com]",
 	"Green Day - Jesus Of Suburbia (2) [MIDIfind.Com]",
 	"Green Dayst Jimmy",
