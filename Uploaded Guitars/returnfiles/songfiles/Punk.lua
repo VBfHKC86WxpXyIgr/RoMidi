f2a35816f7faa57adbefd92e9639b733530af5b4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-13 21:46:47 +0800

    Update Punk index: add "Green Day - Boulevard Of Broken Dreams [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Green Day - American Idiot (2) [MIDIfind.Com]",
+	"Green Day - Boulevard Of Broken Dreams [MIDIfind.Com]",
 	"Green Day - Holiday [MIDIfind.Com]",
 	"Green Day - Jesus Of Suburbia (2) [MIDIfind.Com]",
 }
