c55ceb56bd685d158b7d28a599fbb3106d7d176e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-14 13:36:50 +0800

    Update Metal index: add "Judas Priest - Electric Eye Solo 2nd Part [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -20,6 +20,7 @@ return {
 	"Judas Priest - Diamonds And Rust [MIDIfind.Com]",
 	"Judas Priest - Electric Eye [MIDIfind.Com]",
 	"Judas Priest - Electric Eye Solo 1 [MIDIfind.Com]",
+	"Judas Priest - Electric Eye Solo 2nd Part [MIDIfind.Com]",
 	"Judas Priest - Grinder [MIDIfind.Com]",
 	"Judas Priest - Out In The Cold [MIDIfind.Com]",
 	"Judas Priest - Painkiller [MIDIfind.Com]",
