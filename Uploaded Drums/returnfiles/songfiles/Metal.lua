15729aad69441f9252161d9892bbafcb26c65ddf
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-15 00:14:27 +0800

    Update Metal index: add "Metallica - Battery [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -97,6 +97,7 @@ return {
 	"Megadeth - Holy Wars",
 	"Meshuggah- Bleed.mid",
 	"Metallica - Battery (2) [MIDIfind.com]",
+	"Metallica - Battery [MIDIfind.Com]",
 	"Metallica - Creeping Death (2) [MIDIfind.Com]",
 	"Metallica - Disposable Heroes [MIDIfind.Com]",
 	"Metallica - Dyers Eve [MIDIfind.Com]",
