ae4e235fb98798719fb15761f0de5e7bfa23e9d6
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-14 13:48:18 +0800

    Update Metal index: add "Ratt - Round And Round [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -124,6 +124,7 @@ return {
 	"Rage Against the Machine - Down on the Street [MIDIfind.com]",
 	"Rage Against the Machine - Killing in the Name with drum (1)",
 	"Rage Against the Machine - Rage Against the Machine Know Your Enemy [MIDIfind.com]",
+	"Ratt - Round And Round [MIDIfind.Com]",
 	"Revenga",
 	"Scream",
 	"Slapshock - Direction [MIDIfind.com]",
