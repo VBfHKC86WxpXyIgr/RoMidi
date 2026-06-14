0c7cd0005404a12ebd6c4d1edb621836e862d05e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-14 13:32:07 +0800

    Update Rock index: add "WASP - Sleeping In The Fire [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -59,6 +59,7 @@ return {
 	"The Trooper",
 	"War Pigs - Black Sabbath",
 	"WASP - I Wanna Be Somebody (2) [MIDIfind.Com]",
+	"WASP - Sleeping In The Fire [MIDIfind.Com]",
 	"Weird Fishes Arpeggi - Radiohead Weird Fishes Arpeggi - Radiohead",
 	"Welcome To The Jungle",
 	"What I'Ve Done - Linkin Park",
