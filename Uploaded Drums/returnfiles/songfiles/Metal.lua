880ce073ee2ea891324d5b816e8616465fc07a1a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-14 13:49:07 +0800

    Update Metal index: add "Мастер - Берегись [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -164,5 +164,6 @@ return {
 	"Valkyrie - Heavy Metal Brigade [MIDIfind.com]",
 	"Vega - Heavy Metal Religija [MIDIfind.com]",
 	"WASP - I Wanna Be Somebody (2) [MIDIfind.Com]",
+	"Мастер - Берегись [MIDIfind.Com]",
 	"Мастер - Мастер [MIDIfind.Com]",
 }
