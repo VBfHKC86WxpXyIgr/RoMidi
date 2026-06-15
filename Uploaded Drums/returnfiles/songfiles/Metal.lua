255e306849a01106af5bcb3101e4f26e555e24d6
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-15 13:53:46 +0800

    Update Metal index: add "Metallica - Wherever I May Roam [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -112,6 +112,7 @@ return {
 	"Metallica - Seek And Destroy [MIDIfind.Com]",
 	"Metallica - The Four Horseman [MIDIfind.Com]",
 	"Metallica - Welcome Home Sanatarium [MIDIfind.Com]",
+	"Metallica - Wherever I May Roam [MIDIfind.Com]",
 	"Metallica-Enter-Sandman midi",
 	"Mushroomhead - 12 Hundred [MIDIfind.com]",
 	"Mushroomhead - Episode 29 [MIDIfind.com]",
