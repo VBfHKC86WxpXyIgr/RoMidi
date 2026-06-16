20a603c15b067b4ce273b7d134afc19736248df8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-16 12:22:49 +0800

    Update Others index: add "Seasons - Wave To Earth (Piano)" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -177,6 +177,7 @@ return {
 	"Samplab Screen-20260606-1108",
 	"Screen-20260606-110846 06062026",
 	"Screen-20260606-130710 06062026",
+	"Seasons - Wave To Earth (Piano)",
 	"Seasons (Wave To Earth) (Birru) MIDI",
 	"Seasons-Kim-Seasonswavetoearthseasons",
 	"Shucks Ending V2",
