5931b777c14f629ca30c1cc77e10665609c7b27a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-17 21:58:08 +0800

    Update Metal index: add "Megadeth - Peace Sells [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -96,6 +96,7 @@ return {
 	"Master of Puppets",
 	"Megadeth - Devils Island [MIDIfind.Com]",
 	"Megadeth - Holy Wars",
+	"Megadeth - Peace Sells [MIDIfind.Com]",
 	"Megadeth - Symphony Of Destruction [MIDIfind.Com]",
 	"Megadeth - The Conjuring [MIDIfind.Com]",
 	"Meshuggah- Bleed.mid",
