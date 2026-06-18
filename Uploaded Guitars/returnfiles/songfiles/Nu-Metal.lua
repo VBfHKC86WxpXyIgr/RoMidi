d6cfc5aaa320cf0d38ea5ac0535bba180f9556da
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-18 22:43:11 +0800

    Update Nu-Metal index: add "Deftones - My Own Summer Shove It [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"04-Hypnotize",
+	"Deftones - My Own Summer Shove It [MIDIfind.Com]",
 	"Korn - Blind",
 	"Psychosocial - Slipknot",
 	"System Of A Down - Aerials [MIDIfind.Com]",
