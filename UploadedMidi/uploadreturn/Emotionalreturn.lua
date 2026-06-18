8e7921bc4abb01ff70f2d1f32e9ae76386e3cc2c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-19 05:01:10 +0800

    Update Emotional index: add "Love Story X Golden Brown" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -62,6 +62,7 @@ return {
 	"Like Him Tyler The Creator",
 	"Lost Sky - Dreams Pt. II",
 	"Love Story  Taylor Swift 1775672226231",
+	"Love Story X Golden Brown",
 	"Ludovico Einaudi - Experience",
 	"Marcha Nupcial",
 	"MấT KếT NốI",
