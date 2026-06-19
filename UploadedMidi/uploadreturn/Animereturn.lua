55db601cb09a6c54a481c308156d8a91ece7b25a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-19 21:55:31 +0800

    Update Anime index: add "Reinai Circulation Mhxa" [piano] via uploader

--- UploadedMidi/uploadreturn/Animereturn.lua
+++ UploadedMidi/uploadreturn/Animereturn.lua
@@ -66,6 +66,7 @@ return {
 	"pf solo",
 	"popipo - Hatsune miku - midi by tutogamer2a - WIP",
 	"Promise Neverland - Isabella'S Lullaby Mhxa",
+	"Reinai Circulation Mhxa",
 	"Rokudenashi - One Voice Mhxa",
 	"Scott Joplin'S The Entertainer MIDI FULL",
 	"Self Embodiment of Perfection (Mahito Theme)",
