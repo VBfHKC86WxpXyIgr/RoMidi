452ce9ab5b94fb73b0839790155c9e322d9ece0f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-20 00:36:17 +0800

    Update Metal index: add "VØJ & Narvent - Memory Reboot" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -105,6 +105,7 @@ return {
 	"Type O Negative - My Girlfriend'S Girlfriend (2) [MIDIfind.Com]",
 	"Type O Negative - Out Of The Fire [MIDIfind.Com]",
 	"Vai Steve - For The Love Of God [MIDIfind.Com]",
+	"VØJ & Narvent - Memory Reboot",
 	"WASP - On Your Knees [MIDIfind.Com]",
 	"Мастер - 2000 лет (Иуда) [MIDIfind.Com]",
 	"Мастер - Берегись [MIDIfind.Com]",
