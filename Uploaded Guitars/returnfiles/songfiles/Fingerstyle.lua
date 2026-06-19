283d97ef3a41716f919ca9c475ea1f715e089ed6
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-20 01:45:42 +0800

    Update Fingerstyle index: add "Finnish Folk Song - SäKkijäRven Polkka" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Fingerstyle.lua
+++ Uploaded Guitars/returnfiles/songfiles/Fingerstyle.lua
@@ -3,6 +3,7 @@ return {
 	"Composed By Toby Fox Transcribed By Nefelibata - Home",
 	"Dumb Ways To Die",
 	"Dumb Ways To Die (1)",
+	"Finnish Folk Song - SäKkijäRven Polkka",
 	"Heart-Shaped Box",
 	"Lithium - Nirvana",
 	"One Last Breath",
