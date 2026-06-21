a60b83ec843f1d4f0b0397b59f3310744feaea38
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-21 14:19:30 +0800

    Update Punk index: add "Jesus Doesn'T Want Me For A Sunbeam - Nirvana (Piano-Vocal-Guitar)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -18,6 +18,7 @@ return {
 	"Green Day - Whatsername [MIDIfind.Com]",
 	"Green Day - When I Come Around [MIDIfind.Com]",
 	"Green Dayst Jimmy",
+	"Jesus Doesn'T Want Me For A Sunbeam - Nirvana (Piano-Vocal-Guitar)",
 	"Lake Of Fire - Nirvana (Piano-Vocal-Guitar)",
 	"Nirvana - Big Cheese (4) [MIDIfind.Com]",
 	"Nirvana - Big Long Now",
