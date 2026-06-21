58872d5d4487ffeb1d349aa6d0f3ab8a0c57cc3c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-22 05:30:03 +0800

    Update Metal index: add "Venom - If You Wanna War [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -149,6 +149,7 @@ return {
 	"Type O Negative - Out Of The Fire [MIDIfind.Com]",
 	"Vai Steve - For The Love Of God [MIDIfind.Com]",
 	"Valhalla - Bathory",
+	"Venom - If You Wanna War [MIDIfind.Com]",
 	"VØJ & Narvent - Memory Reboot",
 	"War - Burzum",
 	"WASP - On Your Knees [MIDIfind.Com]",
