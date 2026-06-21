88f60bd95fa05726e51ce008954a75d808a12e6e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-21 12:14:51 +0800

    Update Nu-Metal index: add "Limp Bizkit - Break Stuff (2) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
@@ -4,6 +4,7 @@ return {
 	"Deftones - Engine Number (9) [MIDIfind.Com]",
 	"Deftones - My Own Summer Shove It [MIDIfind.Com]",
 	"Korn - Blind",
+	"Limp Bizkit - Break Stuff (2) [MIDIfind.Com]",
 	"Psychosocial - Slipknot",
 	"System Of A Down - Aerials [MIDIfind.Com]",
 	"System Of A Down - Sugar [MIDIfind.Com]",
