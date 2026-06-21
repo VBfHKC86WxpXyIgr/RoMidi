94e52f4f5ddeb3fdecd32e01b6298121d8ce709d
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-21 08:51:45 +0800

    Update Metal index: add "The Man Who Sold The World - Nirvana" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -183,6 +183,7 @@ return {
 	"System Of A Down - Aerials [MIDIfind.Com]",
 	"System Of A Down - Chop Suey! [MIDIfind.Com]",
 	"System Of A Down - Sugar [MIDIfind.Com]",
+	"The Man Who Sold The World - Nirvana",
 	"The Stage - Avenged Sevenfold by Rgman",
 	"Three Days Grace - I Hate Everything About You [MIDIfind.Com]",
 	"ThunderStruck - ACDC",
