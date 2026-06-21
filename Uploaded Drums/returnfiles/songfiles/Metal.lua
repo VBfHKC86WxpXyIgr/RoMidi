b6053c42bd37b5aa9a0298168ffd5fbccf2de0e8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-21 14:34:27 +0800

    Update Metal index: add "I Hate Myself And Want To Die (Drums + Guitar)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -96,6 +96,7 @@ return {
 	"Guns N' Roses - Sweet Child O' Mine [MIDIfind.com]",
 	"Heart Shaped Box - Nirvana Heart Shaped Box",
 	"HereToStay",
+	"I Hate Myself And Want To Die (Drums + Guitar)",
 	"I Won't See You Tonight - Avenged Sevenfold",
 	"Judas Priest - Breaking The Law [MIDIfind.Com]",
 	"Judas Priest - Diamonds And Rust [MIDIfind.Com]",
