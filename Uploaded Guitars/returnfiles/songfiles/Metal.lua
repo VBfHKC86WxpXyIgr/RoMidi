ebec9d4496ebfbb62bf3b7215cfd2c3cf6bcffa5
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-22 05:30:41 +0800

    Update Metal index: add "Venom - Countess Bathory (2) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -149,6 +149,7 @@ return {
 	"Type O Negative - Out Of The Fire [MIDIfind.Com]",
 	"Vai Steve - For The Love Of God [MIDIfind.Com]",
 	"Valhalla - Bathory",
+	"Venom - Countess Bathory (2) [MIDIfind.Com]",
 	"Venom - Don'T Burn The Witch [MIDIfind.Com]",
 	"Venom - If You Wanna War [MIDIfind.Com]",
 	"Venom - Prime Evil [MIDIfind.Com]",
