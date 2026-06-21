56cabf01f657a01433ea4ebef3d39ed5be5f7af7
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-22 04:49:17 +0800

    Update Metal index: add "Ilia Burzum" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -38,6 +38,7 @@ return {
 	"Dio Ronnie James - Holy Diver [MIDIfind.Com]",
 	"Eruption - Van Halen",
 	"For-Whom-The-Bell-Tolls",
+	"Ilia Burzum",
 	"Jason Becker - Altitudes Solo",
 	"Judas Priest - A Touch Of Evil [MIDIfind.Com]",
 	"Judas Priest - Breaking The Law [MIDIfind.Com]",
