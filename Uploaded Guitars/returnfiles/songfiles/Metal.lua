de3c2cec7a585b7e8b84e51549f8dbef719c268c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-22 06:10:15 +0800

    Update Metal index: add "Silencer - Death Pierce Me [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -150,6 +150,7 @@ return {
 	"Rammstein - Du Hast [MIDIfind.Com]",
 	"Rammstein - Sonne [MIDIfind.Com]",
 	"Roses On Bathory S Blood Bath",
+	"Silencer - Death Pierce Me [MIDIfind.Com]",
 	"Silencer - Sterile Nails And Thunderbowels [MIDIfind.Com]",
 	"Slayer - Angel Of Death (2) [MIDIfind.Com]",
 	"Slayer - Raining Blood [MIDIfind.Com]",
