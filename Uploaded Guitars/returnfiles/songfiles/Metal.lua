39dd8b1bc3c29cddf8760857588ddfa9d272f96f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-23 01:05:37 +0800

    Update Metal index: add "People = Shit - Slipknot" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -146,6 +146,7 @@ return {
 	"Pantera - The Art Of Shredding",
 	"Pantera - This Love [MIDIfind.Com]",
 	"Pantera - Walk (3)",
+	"People = Shit - Slipknot",
 	"Psychonaut 4 - Drop By Drop [MIDIfind.Com]",
 	"Psychonaut 4 - Lethargic Dialogue [MIDIfind.Com]",
 	"Psychonaut 4 - Nackskott [MIDIfind.Com]",
