8e0eb65ac324a00849a9031cc6d8e41e45b2f233
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-22 23:09:54 +0800

    Update Metal index: add "50 Riffs - Iron Maiden (Pro)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -7,6 +7,7 @@ return {
 	"50 Metallica RIFFS",
 	"50 Pantera RIFFS",
 	"50 Riffs - Ac Dc (Pro)",
+	"50 Riffs - Iron Maiden (Pro)",
 	"505 - Arctic Monkeys",
 	"A Miserable Life",
 	"ÁGua De Beber (Tom Jobim), Apenas Solo.Mid",
