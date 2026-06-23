8f3104876a170c0cfab7288e98bbf32fb30c0e92
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-23 18:30:55 +0800

    Update Nu-Metal index: add "Linkin Park - One Step Closer [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
@@ -5,6 +5,7 @@ return {
 	"Deftones - My Own Summer Shove It [MIDIfind.Com]",
 	"Korn - Blind",
 	"Limp Bizkit - Break Stuff (2) [MIDIfind.Com]",
+	"Linkin Park - One Step Closer [MIDIfind.Com]",
 	"Psychosocial - Slipknot",
 	"System Of A Down - Aerials [MIDIfind.Com]",
 	"System Of A Down - Sugar [MIDIfind.Com]",
