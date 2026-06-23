61ea16a6ff3d280aba1a04e77ccf3cbef219ff28
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-24 02:29:55 +0800

    Update Punk index: add "Misfits - Hybrid Moments (3) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -27,6 +27,7 @@ return {
 	"Marigold - Nirvana",
 	"Mexican Seafood - Nirvana Mexican Seafood",
 	"Misfits - Die Die My Darling [MIDIfind.Com]",
+	"Misfits - Hybrid Moments (3) [MIDIfind.Com]",
 	"Misfits - Last Caress [MIDIfind.Com]",
 	"Misfits - Skulls (2) [MIDIfind.Com]",
 	"Misfits - Where Eagles Dare [MIDIfind.Com]",
