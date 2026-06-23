ab46a3770143bcf9df0ab85ffe4fe2142be6b6b9
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-24 02:28:44 +0800

    Update Punk index: add "Misfits - Where Eagles Dare [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -26,6 +26,7 @@ return {
 	"Lake Of Fire - Nirvana (Piano-Vocal-Guitar)",
 	"Marigold - Nirvana",
 	"Mexican Seafood - Nirvana Mexican Seafood",
+	"Misfits - Where Eagles Dare [MIDIfind.Com]",
 	"Negative Creep - Nirvana (Piano-Vocal-Guitar)",
 	"Nirvana - Aero Zeppelin (4) [MIDIfind.Com]",
 	"Nirvana - Big Cheese (4) [MIDIfind.Com]",
