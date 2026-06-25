902a2cefb51d09381ca8d376e5eabcb497ace1fe
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-25 18:48:39 +0800

    Update Classical index: add "바라만-본다--피아노-커버악보포함--Pianocover" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -99,5 +99,6 @@ return {
 	"بروحي فتاة.",
 	"กลัวว่าฉันจะไม่เสียใจ (Fear) – PURPEECH",
 	"거리에서Sung-Si-Kyung---On-The-Street-Piano-Ver",
+	"바라만-본다--피아노-커버악보포함--Pianocover",
 	"카트라이더 대저택 Bgm 피아노 청음연주 + 중급자용 악보 (Mansion Dance Battle Piano Cover) [문호준 인트로] - 지훈피아노 JH Piano (192k)",
 }
