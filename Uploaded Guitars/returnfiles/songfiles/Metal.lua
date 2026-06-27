df10e59c90eab0fc4cdae96133592c582a82e885
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-27 18:34:39 +0800

    Update Metal index: add "Metallica - To Live Is To Die (11) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -157,6 +157,7 @@ return {
 	"Metallica - The Struggle Within [MIDIfind.Com]",
 	"Metallica - The Thing That Should Not Be [MIDIfind.Com]",
 	"Metallica - Through The Never [MIDIfind.Com]",
+	"Metallica - To Live Is To Die (11) [MIDIfind.Com]",
 	"Metallica - Trapped Under Ice [MIDIfind.Com]",
 	"Metallica - Welcome Home Sanatarium [MIDIfind.Com]",
 	"Metallica - Wherever I May Roam [MIDIfind.Com]",
