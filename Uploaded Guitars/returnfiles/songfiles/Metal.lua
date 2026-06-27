bd46676469a3af3d644692a49ac4673d643ff067
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-27 19:06:28 +0800

    Update Metal index: add "Metallica - 2 X 4 [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -106,6 +106,7 @@ return {
 	"Megadeth - Symphony Of Destruction [MIDIfind.Com]",
 	"Megadeth - The Conjuring [MIDIfind.Com]",
 	"Megadeth - Tornado Of Souls [MIDIfind.Com]",
+	"Metallica - 2 X 4 [MIDIfind.Com]",
 	"Metallica - Am I Evil [MIDIfind.Com]",
 	"Metallica - And Justice For All [MIDIfind.Com]",
 	"Metallica - Anesthesia Pulling Teeth With Wah Sound",
