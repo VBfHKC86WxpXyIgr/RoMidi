5ab7c1b2d8804314d7c593ff35e513ad690ac841
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-27 22:37:12 +0800

    Update Others index: add "AA-Isaac Newton" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -15,6 +15,7 @@ return {
 	"50 Metallica RIFFS",
 	"50 Nirvana RIFFS",
 	"60375 Purnama-Merindu-",
+	"AA-Isaac Newton",
 	"Ai♡Scream Mhxa",
 	"AKASAKI - Bunny Girl Mhxa",
 	"ALL MY FELLAS [ Remastered & Finished ]",
