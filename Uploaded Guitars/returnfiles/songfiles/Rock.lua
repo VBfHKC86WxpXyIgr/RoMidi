68249e703c88810f6c2fc1987c7c4f5d5657a576
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-28 20:52:31 +0800

    Update Rock index: add "AUD DW0234" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -8,6 +8,7 @@ return {
 	"Alice In Chains - Sludge Factory [MIDIfind.Com]",
 	"Alice In Chains - Would(3) [MIDIfind.Com]",
 	"American Football - Never Meant",
+	"AUD DW0234",
 	"Beggin-Maneskin.Mid",
 	"Bismarck - Sabaton Piano Arrangement",
 	"Black Rover - Black Clover (OP 3)",
