1c44b519a5808e76730a4442851514d3f8ef6eb0
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 07:00:40 +0800

    Update Punk index: add "Green Day - Last Night On Earth" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -16,6 +16,7 @@ return {
 	"Green Day - Holiday [MIDIfind.Com]",
 	"Green Day - Homecoming [MIDIfind.Com]",
 	"Green Day - Jesus Of Suburbia (2) [MIDIfind.Com]",
+	"Green Day - Last Night On Earth",
 	"Green Day - Letterbomb [MIDIfind.Com]",
 	"Green Day - Longview [MIDIfind.Com]",
 	"Green Day - She'S A Rebel [MIDIfind.Com]",
