f089e4293cde397ac77a9a86ebf396d4701aed3c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 00:06:36 +0800

    Update Emotional index: add "Seasons-In-The-Sun-Terry-Jacks-Ballad-Piano-Level-4 1" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -92,6 +92,7 @@ return {
 	"Risk It All  Bruno Mars 1776838404638",
 	"River Flows In You",
 	"Seasons In The Sun - Westlife (Piano-Voice)",
+	"Seasons-In-The-Sun-Terry-Jacks-Ballad-Piano-Level-4 1",
 	"Self Aware - Temper City (Piano)",
 	"Sinking Town   沈める街 - Yoeko Kurahashi",
 	"Sunsezt yt @moongate 1234",
