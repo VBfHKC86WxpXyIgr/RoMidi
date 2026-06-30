f3b6441367e8715eb3db75a254213f297c20da42
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 23:30:02 +0800

    Update Indie index: add "Mac Demarco - Freaking Out The Neighborhood [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Indie.lua
+++ Uploaded Guitars/returnfiles/songfiles/Indie.lua
@@ -3,5 +3,6 @@ return {
 	"Boys Don'T Cry - The Cure (Piano Arrangement)",
 	"Creep - Radiohead (Guitar)",
 	"Led Zeppelin-Stairway To Heaven-06-11-2026 (1)",
+	"Mac Demarco - Freaking Out The Neighborhood [MIDIfind.Com]",
 	"SpongeBob - Main Theme [MIDIfind.Com]",
 }
