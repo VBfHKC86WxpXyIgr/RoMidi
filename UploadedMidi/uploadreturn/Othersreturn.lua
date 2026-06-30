68b93c86484960f939e12fecd1e5ee69f77217f8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 14:34:52 +0800

    Update Others index: add "DONT+TOUCH+MY+PIZZA" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -103,6 +103,7 @@ return {
 	"đôNg MiêM",
 	"DòNg TháC ThờI Gian",
 	"DONT TOUCH MY PIZZA",
+	"DONT+TOUCH+MY+PIZZA",
 	"Drowning Love - Chasing Kou Mhxa",
 	"Dù Cho TậN Thế",
 	"đừNg Bỏ Em 1 MìNh",
