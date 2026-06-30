6a49b34921cf422357f257cfa0385d17b11f934f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 23:01:46 +0800

    Update Metal index: add "Metallica - The Call Of Ktulu [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -164,6 +164,7 @@ return {
 	"Metallica - Some Kind Of Monster [MIDIfind.Com]",
 	"Metallica - Spit Out The Bone",
 	"Metallica - St Anger [MIDIfind.Com]",
+	"Metallica - The Call Of Ktulu [MIDIfind.Com]",
 	"Metallica - The Four Horseman [MIDIfind.Com]",
 	"Metallica - The Unforgiven [MIDIfind.Com]",
 	"Metallica - Trapped Under Ice [MIDIfind.Com]",
