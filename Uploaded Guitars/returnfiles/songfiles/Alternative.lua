38080f690b41b0b476ffac40969e5dd0ce671c3f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 23:28:46 +0800

    Update Alternative index: add "Audioslave - Like A Stone [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Alternative.lua
+++ Uploaded Guitars/returnfiles/songfiles/Alternative.lua
@@ -2,6 +2,7 @@
 return {
 	"46792 Buko-",
 	"Aerosmith - Dream On",
+	"Audioslave - Like A Stone [MIDIfind.Com]",
 	"Bjork - Army Of Me WWW.MIDISFREE.COM",
 	"Chase Batta - JoJo'S Bizarre Adventure Part4 Diamond Is Unbreakable OP2",
 	"Chevelle - Comfortable Liar [MIDIfind.Com]",
