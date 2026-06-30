132e7c56ebc4f9ebceda4398a3f7806df1e6f87d
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-06-30 10:22:02 +0800

    Update Modern index: add "DạO BướC HongKong 1999" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -33,6 +33,7 @@ return {
 	"Credits Song For My Death (Code: 900)",
 	"Dandelions - Ruth B. 2",
 	"Dandelions.com",
+	"DạO BướC HongKong 1999",
 	"Dark Red",
 	"Daylight - David Kushner",
 	"Die with a smile - Piano Solo LKZ",
