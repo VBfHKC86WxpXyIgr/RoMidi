d2c8ea6227746a048f343dfe3f1dc8a0fc0ef876
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-02 07:15:51 +0800

    Update Funk index: add "Crazy Frog - Axel F.Mid" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Funk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Funk.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Crazy Frog - Axel F.Mid",
 	"Limp Bizkit - Take A Look Around [MIDIfind.Com]",
 	"Motley Crue - Kickstart My Heart.Mid",
 	"Кино - Последний Герой Kino - The Last Hero",
