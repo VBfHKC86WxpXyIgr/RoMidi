ca859027a85e2d1ac610d37da3316a1b915bbf03
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-01 12:26:57 +0800

    Update Classic Rock index: add "Eagles (The) - Hotel California (3) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -6,6 +6,7 @@ return {
 	"Buddy Holly - Weezer (Extremely Accurate) 🔥🎸G",
 	"Cant Stop - Red Hot Chili Peppers",
 	"Children",
+	"Eagles (The) - Hotel California (3) [MIDIfind.Com]",
 	"Fingerstyle",
 	"GOAT - Polyphia G O A T - Copy Mscz",
 	"Guns N Roses - Sweet Child O Mine",
