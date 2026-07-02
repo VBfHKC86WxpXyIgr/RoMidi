5a6fd3f1318f52b2f718eea09472b3a8f135c981
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-03 05:39:39 +0800

    Update Funk index: add "Metallica-Enter-Sandman Midi" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Funk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Funk.lua
@@ -2,6 +2,7 @@
 return {
 	"Crazy Frog - Axel F.Mid",
 	"Limp Bizkit - Take A Look Around [MIDIfind.Com]",
+	"Metallica-Enter-Sandman Midi",
 	"Motley Crue - Kickstart My Heart.Mid",
 	"Ozzy Osbourne - Crazy Train.Mid",
 	"Кино - Последний Герой Kino - The Last Hero",
