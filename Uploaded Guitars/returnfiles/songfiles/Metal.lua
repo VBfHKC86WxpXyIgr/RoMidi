39e091f2d4e8e5e235139e3d059e172e40729f79
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-03 19:08:54 +0800

    Update Metal index: add "Slipknot - Duality [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -234,6 +234,7 @@ return {
 	"Slayer - Angel Of Death (2) [MIDIfind.Com]",
 	"Slayer - Raining Blood [MIDIfind.Com]",
 	"Slipknot - Before I Forget [MIDIfind.Com]",
+	"Slipknot - Duality [MIDIfind.Com]",
 	"Slipknot - The Heretic Anthem (4) [MIDIfind.Com]",
 	"Stabwound - Necrophagist Piano Arrangement (Sorta) Lyrics!!",
 	"Symphony X - Sea Of Lies First Part",
