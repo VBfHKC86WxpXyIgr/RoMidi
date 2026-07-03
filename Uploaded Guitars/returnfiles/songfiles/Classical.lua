d0d72313197efdde5b21a101c8673737e9957737
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 01:05:52 +0800

    Update Classical index: add "Beethoven - Moonlight Sonata (3) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classical.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classical.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Beethoven - Moonlight Sonata (3) [MIDIfind.Com]",
 	"FrankSinatra My Way",
 	"Someone Like You (Easy Piano)",
 }
