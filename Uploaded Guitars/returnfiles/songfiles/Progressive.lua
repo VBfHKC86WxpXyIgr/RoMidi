00c3ac372f27bc82a30424a25119bceaa0fe0b82
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 18:50:22 +0800

    Update Progressive index: add "Misc Covers-Unslept (Accurate Pitch) By Miraidempa-07-04-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Progressive.lua
+++ Uploaded Guitars/returnfiles/songfiles/Progressive.lua
@@ -4,6 +4,7 @@ return {
 	"Misc Covers-Made Your Mark-07-04-2026",
 	"Misc Covers-Poppies Lane Memoryy By Slank",
 	"Misc Covers-Sempurna By Andra And The Backbone-07-04-2026",
+	"Misc Covers-Unslept (Accurate Pitch) By Miraidempa-07-04-2026",
 	"Sayonara Cross Teck-07-04-2026",
 	"Smashing Pumpkins - 1979",
 	"Wonderless - Pierce The Veil",
