f633ed7b8aafe2bfcf790d202cab921685ffb850
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 22:44:34 +0800

    Update Memes index: add "Misc Covers-Aruarian Dance - Simplified By Nujabes-07-04-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Memes.lua
+++ Uploaded Guitars/returnfiles/songfiles/Memes.lua
@@ -2,6 +2,7 @@
 return {
 	"Home - Were Finally Landing 65702210",
 	"Michael Jackson - Billie Jean",
+	"Misc Covers-Aruarian Dance - Simplified By Nujabes-07-04-2026",
 	"Valve HALF LIFE 2 OST (Www.Lightaudio.Ru)",
 	"Гимны - Гимн СССР России [MIDIfind.Com]",
 	"Народные песни - Походный марш Люфтваффе (Немецкий) [MIDIfind.Com]",
