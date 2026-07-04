980ba9a1f30fc5ffedf77dd0db151f0e63679396
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 22:43:27 +0800

    Update OST index: add "Misc Covers-Jotaro'S Theme By JJBA-07-04-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/OST.lua
+++ Uploaded Guitars/returnfiles/songfiles/OST.lua
@@ -3,6 +3,7 @@ return {
 	"Dire, Dire Docks (Super Mario 64)",
 	"Ella Baila Sola Eslabon Armado Y Peso Pluma",
 	"Grand Theft Auto San Andreas - Intro [MIDIfind.Com]",
+	"Misc Covers-Jotaro'S Theme By JJBA-07-04-2026",
 	"OFFICIAL Roblox Bakon Theme Song",
 	"Seek (Among Us) NEW",
 	"The Legend Of Zelda Great Fairy Fountain (Piano Cover)",
