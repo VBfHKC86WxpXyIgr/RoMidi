4fb13c3a23cb07dad7c50298ca91c55883accb66
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-05 01:01:57 +0800

    Update Emotional index: add "My Chemical Romance - I Don'T Love You" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -87,6 +87,7 @@ return {
 	"Mrs Magic - Strawberry Guy (Intro)",
 	"Mrs Magic - Strawberry Guy (Piano Version)",
 	"Multo - Cup of Joe RNE",
+	"My Chemical Romance - I Don'T Love You",
 	"Như có Bác trong ngày Đại thắng impossible",
 	"Nightchanges by dc ; @jvkowi",
 	"Oceans & Engines",
