14233b0f8871c3f03bcc0e5b4b4c4e51177b977f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 18:49:15 +0800

    Update Progressive index: add "Misc Covers-Made Your Mark-07-04-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Progressive.lua
+++ Uploaded Guitars/returnfiles/songfiles/Progressive.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Misc Covers-Made Your Mark-07-04-2026",
 	"Sayonara Cross Teck-07-04-2026",
 	"Wonderless - Pierce The Veil",
 }
