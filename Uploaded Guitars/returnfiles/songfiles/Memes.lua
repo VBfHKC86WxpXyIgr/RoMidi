31badc172063eff5e62136c09102a5acf3fa7cc8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-05 00:14:35 +0800

    Update Memes index: add "Misc Covers-Ripped Pants By Spongebob Squarepants And The Losers-07-04-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Memes.lua
+++ Uploaded Guitars/returnfiles/songfiles/Memes.lua
@@ -3,6 +3,7 @@ return {
 	"Home - Were Finally Landing 65702210",
 	"Michael Jackson - Billie Jean",
 	"Misc Covers-Aruarian Dance - Simplified By Nujabes-07-04-2026",
+	"Misc Covers-Ripped Pants By Spongebob Squarepants And The Losers-07-04-2026",
 	"Valve HALF LIFE 2 OST (Www.Lightaudio.Ru)",
 	"Гимны - Гимн СССР России [MIDIfind.Com]",
 	"Народные песни - Походный марш Люфтваффе (Немецкий) [MIDIfind.Com]",
