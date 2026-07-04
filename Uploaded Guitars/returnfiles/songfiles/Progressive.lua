99693bdc1f1fb3281f0ff4d2b15a421543e1a2e4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 18:49:37 +0800

    Update Progressive index: add "Misc Covers-Poppies Lane Memoryy By Slank" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Progressive.lua
+++ Uploaded Guitars/returnfiles/songfiles/Progressive.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Misc Covers-Made Your Mark-07-04-2026",
+	"Misc Covers-Poppies Lane Memoryy By Slank",
 	"Sayonara Cross Teck-07-04-2026",
 	"Wonderless - Pierce The Veil",
 }
