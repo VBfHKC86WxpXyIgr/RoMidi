b92cf9ddc41e2ed743bfb4b6c2f66b4385049e07
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-04 22:44:19 +0800

    Update Progressive index: add "Misc Covers-Restless By Untitled-07-04-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Progressive.lua
+++ Uploaded Guitars/returnfiles/songfiles/Progressive.lua
@@ -3,6 +3,7 @@ return {
 	"Misc Covers-Loneliness By Decalius-07-04-2026",
 	"Misc Covers-Made Your Mark-07-04-2026",
 	"Misc Covers-Poppies Lane Memoryy By Slank",
+	"Misc Covers-Restless By Untitled-07-04-2026",
 	"Misc Covers-Sempurna By Andra And The Backbone-07-04-2026",
 	"Misc Covers-Unslept (Accurate Pitch) By Miraidempa-07-04-2026",
 	"Sayonara Cross Teck-07-04-2026",
