3d7080d91afc70483f78ce725de289ec94540d99
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-06 00:47:25 +0800

    Update Jazz index: add "Vaughan Stevie Ray - Pride And Joy [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Jazz.lua
+++ Uploaded Drums/returnfiles/songfiles/Jazz.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"B.B King - The Thrill Is Gone [MIDIfind.Com]",
+	"Vaughan Stevie Ray - Pride And Joy [MIDIfind.Com]",
 	"Whiplash - Caravan",
 	"Whiplash-Caravan By B.F.",
 }
