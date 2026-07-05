4ca57361ff6856adaf1c3f5836baf552ef01cb19
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-05 08:05:38 +0800

    Update Pop index: add "Buong Doi Tay Nhau Ra" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Pop.lua
+++ Uploaded Guitars/returnfiles/songfiles/Pop.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Blinding Lights - The Weeknd (Piano-Vocal-Guitar)",
+	"Buong Doi Tay Nhau Ra",
 	"ChúNg Ta KhôNg ThuộC Về Nhau",
 	"COME MY WAY",
 	"CơN MưA Ngang Qua 2",
