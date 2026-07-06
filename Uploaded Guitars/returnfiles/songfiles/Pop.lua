f0be34349e79a8c851a6a01ee0f6f18ca9d2335e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-06 14:38:23 +0800

    Update Pop index: add "Cause I Love You - Noo Phuoc Thinh Piano" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Pop.lua
+++ Uploaded Guitars/returnfiles/songfiles/Pop.lua
@@ -2,6 +2,7 @@
 return {
 	"Blinding Lights - The Weeknd (Piano-Vocal-Guitar)",
 	"Buong Doi Tay Nhau Ra",
+	"Cause I Love You - Noo Phuoc Thinh Piano",
 	"ChúNg Ta KhôNg ThuộC Về Nhau",
 	"COME MY WAY",
 	"CơN MưA Ngang Qua 2",
