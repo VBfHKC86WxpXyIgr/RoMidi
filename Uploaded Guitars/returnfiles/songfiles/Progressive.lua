71422cbfb02a5fec8e519f1a2bbaaaccfbfb0877
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-07 02:31:32 +0800

    Update Progressive index: add "Sarah By Alex G" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Progressive.lua
+++ Uploaded Guitars/returnfiles/songfiles/Progressive.lua
@@ -7,6 +7,7 @@ return {
 	"Misc Covers-Restless By Untitled-07-04-2026",
 	"Misc Covers-Sempurna By Andra And The Backbone-07-04-2026",
 	"Misc Covers-Unslept (Accurate Pitch) By Miraidempa-07-04-2026",
+	"Sarah By Alex G",
 	"Sayonara Cross Teck-07-04-2026",
 	"Smashing Pumpkins - 1979",
 	"Wonderless - Pierce The Veil",
