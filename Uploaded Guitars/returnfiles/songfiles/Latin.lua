6598e6101611a923bfb388af68e2bd387002d21a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-07 02:29:52 +0800

    Update Latin index: add "La Resaka By Supermerk2" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Latin.lua
+++ Uploaded Guitars/returnfiles/songfiles/Latin.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"La Resaka By Supermerk2",
 	"Misc Covers-Beretta By El De Las R'S.-07-04-2026",
 	"Misc Covers-La Cheyenne By El De Las R’S-07-04-2026",
 }
