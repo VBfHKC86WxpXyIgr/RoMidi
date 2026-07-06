5ee47183dba9672ddf49024505fd9c6ba46484b8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-07 02:30:03 +0800

    Update Latin index: add "Desvelado By Bobby Pulido" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Latin.lua
+++ Uploaded Guitars/returnfiles/songfiles/Latin.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Desvelado By Bobby Pulido",
 	"La Resaka By Supermerk2",
 	"Misc Covers-Beretta By El De Las R'S.-07-04-2026",
 	"Misc Covers-La Cheyenne By El De Las R’S-07-04-2026",
