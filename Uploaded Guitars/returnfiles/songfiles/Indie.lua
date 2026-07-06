1ea586cd3894024a611ffe1eb3ea836af4a178a0
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-06 18:15:19 +0800

    Update Indie index: add "Angus And Julia Stone - Big Jet Plane [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Indie.lua
+++ Uploaded Guitars/returnfiles/songfiles/Indie.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Angus And Julia Stone - Big Jet Plane [MIDIfind.Com]",
 	"Boys Don'T Cry - The Cure (Piano Arrangement)",
 	"Brubeck Dave - Take Five [MIDIfind.Com]",
 	"Creep - Radiohead (Guitar)",
