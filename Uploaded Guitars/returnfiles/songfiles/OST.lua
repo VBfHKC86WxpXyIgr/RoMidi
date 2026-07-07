6899e6c7a7a968014ef434171b73742f16d8b91f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-07 17:09:37 +0800

    Update OST index: add "Metro Exodus - Race Against Fate (Fingerstyle) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/OST.lua
+++ Uploaded Guitars/returnfiles/songfiles/OST.lua
@@ -4,6 +4,7 @@ return {
 	"Ella Baila Sola Eslabon Armado Y Peso Pluma",
 	"Grand Theft Auto San Andreas - Intro [MIDIfind.Com]",
 	"Metro Exodus - A New Home (Guitar Cover) [MIDIfind.Com]",
+	"Metro Exodus - Race Against Fate (Fingerstyle) [MIDIfind.Com]",
 	"Metro Exodus - Theme From The Main Menu.Gtr",
 	"Misc Covers-Full Theme By JoJo Steel Ball Run-07-04-2026",
 	"Misc Covers-Jotaro'S Theme By JJBA-07-04-2026",
