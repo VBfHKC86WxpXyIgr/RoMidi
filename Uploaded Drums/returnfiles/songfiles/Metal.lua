b5b8883c73f31bb79bd8299e5c2905346306adb3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-08 22:46:37 +0800

    Update Metal index: add "HIM - Razorblade Kiss [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -114,6 +114,7 @@ return {
 	"HIM - Gone With The Sin [MIDIfind.Com]",
 	"HIM - Join Me [MIDIfind.Com]",
 	"HIM - Poison Girl [MIDIfind.Com]",
+	"HIM - Razorblade Kiss [MIDIfind.Com]",
 	"I Hate Myself And Want To Die (Drums + Guitar)",
 	"I Won't See You Tonight - Avenged Sevenfold",
 	"Judas Priest - Breaking The Law [MIDIfind.Com]",
