07a8ad47995a28e37a883c74a7df7a22f174dea4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-09 03:30:00 +0800

    Update Jazz index: add "Caravan(1)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Jazz.lua
+++ Uploaded Drums/returnfiles/songfiles/Jazz.lua
@@ -2,6 +2,7 @@
 return {
 	"A.JACKSON.Livin On Love",
 	"B.B King - The Thrill Is Gone [MIDIfind.Com]",
+	"Caravan(1)",
 	"Vaughan Stevie Ray - Pride And Joy [MIDIfind.Com]",
 	"Whiplash - Caravan",
 	"Whiplash-Caravan By B.F.",
