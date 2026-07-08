f1b38d364d99b70723bd4f23498b298ca613d21c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-08 22:45:17 +0800

    Update Metal index: add "HIM - Poison Girl [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -82,6 +82,7 @@ return {
 	"HermoðR Á Helferð - Burzum",
 	"HIM - Gone With The Sin [MIDIfind.Com]",
 	"HIM - Join Me [MIDIfind.Com]",
+	"HIM - Poison Girl [MIDIfind.Com]",
 	"Ilia Burzum",
 	"Jason Becker - Altitudes Solo",
 	"Judas Priest - A Touch Of Evil [MIDIfind.Com]",
