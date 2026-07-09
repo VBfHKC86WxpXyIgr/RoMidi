320d56683234f50a5558a1c64ed7f857e945f135
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-09 18:41:00 +0800

    Update Metal index: add "Pantera - Revolution Is My Name [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -220,6 +220,7 @@ return {
 	"Pantera - I'M Broken [MIDIfind.Com]",
 	"Pantera - Primal Concrete Sledge [MIDIfind.Com]",
 	"Pantera - Psycho Holiday [MIDIfind.Com]",
+	"Pantera - Revolution Is My Name [MIDIfind.Com]",
 	"Pantera - Shedding Skin [MIDIfind.Com]",
 	"Pantera - Slaughtered [MIDIfind.Com]",
 	"Pantera - Strenght Beyond Strenght [MIDIfind.Com]",
