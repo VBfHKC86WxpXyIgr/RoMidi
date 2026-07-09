9f19afeb7e9898de777d8008c0e03e3255ee96d4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-09 18:17:52 +0800

    Update Metal index: add "Metallica - Master Of Puppets (23) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -157,6 +157,7 @@ return {
 	"Metallica - Leper Messiah [MIDIfind.Com]",
 	"Metallica - Lux ÆTerna",
 	"Metallica - Master Of Puppets",
+	"Metallica - Master Of Puppets (23) [MIDIfind.Com]",
 	"Metallica - Master Of Puppets [MIDIfind.Com]",
 	"Metallica - Metal Militia [MIDIfind.Com]",
 	"Metallica - Motorbreath (2) [MIDIfind.Com]",
