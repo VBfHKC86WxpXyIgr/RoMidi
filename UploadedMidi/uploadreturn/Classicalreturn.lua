be758b2b869dc54b49962e2cb52786c18d4113b8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-09 18:14:28 +0800

    Update Classical index: add "RUSH E SMBs Version Pre-Remaster" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -112,6 +112,7 @@ return {
 	"Rush E",
 	"Rush E but its as difficult as humanly possible",
 	"RUSH E FINAL",
+	"RUSH E SMBs Version Pre-Remaster",
 	"SAMSUNG Over The Horizon 2021 (1)",
 	"Seluruh Nafas Ini Last Child 1780246231273",
 	"She Neva Knows - Justatee She Neva Knows",
