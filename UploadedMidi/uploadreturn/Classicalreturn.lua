205b0ea6353bcac63f0042e22e7144281a903f78
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-11 03:31:54 +0800

    Update Classical index: add "Ticking" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -129,6 +129,7 @@ return {
 	"Tetris Block Arts V4",
 	"The Amazing Digital Circus - Main Theme But Its By HANS ZIMMER EPIC VERSION (Your New Home) - Carameii (192k)",
 	"The-Flight-Of-The-Bumble-Bee",
+	"Ticking",
 	"Tonight",
 	"Vierra-Rasa-Ini-Anonymous-20200323154206-Nonstop2k.Com",
 	"Virginio Aiello, On Piano - Van Gogh",
