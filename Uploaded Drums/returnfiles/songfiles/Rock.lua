b224038b8aff96dfb1c93c526520db361d98e490
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-10 11:10:34 +0800

    Update Rock index: add "THE BEATLES.All My Loving" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Rock.lua
+++ Uploaded Drums/returnfiles/songfiles/Rock.lua
@@ -105,6 +105,7 @@ return {
 	"Skillet - Monster [MIDIfind.Com]",
 	"Smells Like Teen Spirit - Drums",
 	"The Avengers Theme Song (CHECK MY NEW VERSION)",
+	"THE BEATLES.All My Loving",
 	"THE BEATLES.Helter Skelter",
 	"Van Halen - Ain'T Talkin Bout Love (2) [MIDIfind.Com]",
 	"Weezer - Undone the Sweater Song [MIDIfind.com]",
