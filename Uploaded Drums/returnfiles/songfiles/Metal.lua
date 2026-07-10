1f1412d2bafda3973f4592149aac98af7773648a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-11 02:37:36 +0800

    Update Metal index: add "Megadeth-Rust In Peace Polaris" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -144,6 +144,7 @@ return {
 	"Megadeth - Symphony Of Destruction [MIDIfind.Com]",
 	"Megadeth - The Conjuring [MIDIfind.Com]",
 	"Megadeth - Tornado Of Souls [MIDIfind.Com]",
+	"Megadeth-Rust In Peace Polaris",
 	"Meshuggah- Bleed.mid",
 	"Metallica - Am I Evil [MIDIfind.Com]",
 	"Metallica - And Justice For All [MIDIfind.Com]",
