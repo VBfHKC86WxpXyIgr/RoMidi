ba0583dc5e9ce444955500cb682a4f914fd620c7
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-11 11:39:26 +0800

    Update Classic Rock index: add "SurfinUSA" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classic Rock.lua
@@ -38,6 +38,7 @@ return {
 	"Queen - Bohemian Rhapsody Complete [MIDIfind.Com]",
 	"Santana Carlos - Smooth [MIDIfind.Com]",
 	"Shiki No Uta (1)",
+	"SurfinUSA",
 	"The Beatles - A Hard Day'S Night",
 	"THE BEATLES.Blackbird K",
 	"THE BEATLES.Helter Skelter",
