ab29b5f6db9d840259f891732c03852a99a52111
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-12 11:06:20 +0800

    Update Metal index: add "Helloween - Future World (3) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -83,6 +83,7 @@ return {
 	"Happy Days - Don'T Guns (Guitar)",
 	"Haunted (The) - Silencer [MIDIfind.Com]",
 	"Helloween - Dr Stein (2) [MIDIfind.Com]",
+	"Helloween - Future World (3) [MIDIfind.Com]",
 	"HermoðR Á Helferð - Burzum",
 	"HIM - Gone With The Sin [MIDIfind.Com]",
 	"HIM - Join Me [MIDIfind.Com]",
