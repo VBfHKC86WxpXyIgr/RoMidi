f2f45814dc89df10c7682019fe3d6c881ec62a6e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-12 08:51:30 +0800

    Update Metal index: add "Osbourne Ozzy - Walk On Water [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -212,6 +212,7 @@ return {
 	"Osbourne Ozzy - Mr Crowley [MIDIfind.Com]",
 	"Osbourne Ozzy - No More Tears [MIDIfind.Com]",
 	"Osbourne Ozzy - Perry Mason [MIDIfind.Com]",
+	"Osbourne Ozzy - Walk On Water [MIDIfind.Com]",
 	"Ozzy Osbourne-Crazy Train",
 	"Pantera - 10'S [MIDIfind.Com]",
 	"Pantera - 5 Minutes Alone [MIDIfind.Com]",
