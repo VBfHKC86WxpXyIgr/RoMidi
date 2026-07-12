3e65b71077f8ec463068aee8cd943b34095b2ffb
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-12 08:31:23 +0800

    Update Classical index: add "YTDown.Com YouTube Erika-A-German-WW2-Marching-Song Media WGw4mn2-ILc 001 1080p+(1)-1" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -139,6 +139,7 @@ return {
 	"Wedding-Day-at-Troldhaugen",
 	"Woke Up In Japan By Friqtao",
 	"You'Ve Gotta Move",
+	"YTDown.Com YouTube Erika-A-German-WW2-Marching-Song Media WGw4mn2-ILc 001 1080p+(1)-1",
 	"Yung Kai - Blue Piano Cover Sheet Music (1)",
 	"Кино - Группа крови [MIDIfind.Com]",
 	"Руки вверх - 18 мне уже [MIDIfind.Com]",
