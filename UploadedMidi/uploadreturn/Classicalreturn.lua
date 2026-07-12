406192a9c73d8aea52fe4acff5929a398ba59104
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-12 19:04:40 +0800

    Update Classical index: add "[Embers Midi Blacker] RUSH E 5 (WITH 1,000,000 NOTES)" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"[Black MIDI] RUSH E",
+	"[Embers Midi Blacker] RUSH E 5 (WITH 1,000,000 NOTES)",
 	"💞...🪽부활(Boowhal) - Never Ending Story (Piano Cover) [가사LYRICS KOR] - Pianist Cho Younghoon／HOON TO-BE (192k)",
 	"007amanha Nuncamais2",
 	"93772 Imposter-Syndrome-",
