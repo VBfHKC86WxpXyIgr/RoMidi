c9c3bc536029caf0812f3c4b2dc7417e4cbc8407
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-12 11:09:24 +0800

    Update Metal index: add "Racer X - Scarified (2) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -256,6 +256,7 @@ return {
 	"Psychonaut 4 - Overdose Was The Best Way To Die [MIDIfind.Com]",
 	"Psychonaut 4 - Serial Lier (2) [MIDIfind.Com]",
 	"Psychonaut 4 - World Of Pain And Hate [MIDIfind.Com]",
+	"Racer X - Scarified (2) [MIDIfind.Com]",
 	"Rage Against The Machine - Killing In The Name (2) [MIDIfind.Com]",
 	"Rage Against The Machine - Killing In The Name [MIDIfind.Com]",
 	"RainingBlood",
