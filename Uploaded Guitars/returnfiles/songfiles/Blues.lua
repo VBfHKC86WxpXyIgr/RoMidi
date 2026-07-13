463dd0157b77d4cda1347c0473e1ece0e5eadfd0
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 03:05:59 +0800

    Update Blues index: add "1Main Serong By The Changcuters" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Blues.lua
+++ Uploaded Guitars/returnfiles/songfiles/Blues.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"1Juicebox By The Strokes",
+	"1Main Serong By The Changcuters",
 	"Boogie-Woogie-Blues",
 	"Prince - Purple Rain [MIDIfind.Com]",
 }
