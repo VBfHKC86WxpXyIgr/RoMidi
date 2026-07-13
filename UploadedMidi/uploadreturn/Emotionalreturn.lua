43c571b70904d370d7be3e0c910a98d221eed8d2
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-13 19:06:37 +0800

    Update Emotional index: add "Across The Sea - Weezer" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -11,6 +11,7 @@ return {
 	"505 best part",
 	"81416 Take-Me-Back-Home",
 	"A Miserable Life",
+	"Across The Sea - Weezer",
 	"After The Love Has Gone  David Foster Earth Wind  Fire 1775672109595",
 	"Alec Benjamin - Let Me Down Slowly",
 	"Anybody can find love (except you.) - Full Song",
