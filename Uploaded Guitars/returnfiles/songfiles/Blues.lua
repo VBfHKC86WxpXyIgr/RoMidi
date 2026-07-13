dec5895de44f1a2c35fc21fdaa478890216fde6c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 03:26:09 +0800

    Update Blues index: add "1 V2 Main Serong By The Changcuters" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Blues.lua
+++ Uploaded Guitars/returnfiles/songfiles/Blues.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"1 V2 Main Serong By The Changcuters",
 	"1Juicebox By The Strokes",
 	"1Main Serong By The Changcuters",
 	"Boogie-Woogie-Blues",
