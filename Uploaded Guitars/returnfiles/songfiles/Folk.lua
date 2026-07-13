d155152e472eae2090e8e680d5d767f71e3dfbe7
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 05:25:03 +0800

    Update Folk index: add "House-Of-The-Rising-Sun-1.Mid" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Folk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Folk.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"1Hava Nagila",
+	"House-Of-The-Rising-Sun-1.Mid",
 	"Misc Covers-Foggy Mountain Breakdown-07-04-2026",
 	"Misc Covers-You'Ve Gotta Move-07-04-2026",
 }
