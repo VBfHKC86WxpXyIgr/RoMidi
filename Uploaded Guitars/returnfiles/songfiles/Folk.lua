1f8746e12387835c5a5b581a313928e0490e2546
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 04:21:20 +0800

    Update Folk index: add "1Hava Nagila" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Folk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Folk.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"1Hava Nagila",
 	"Misc Covers-Foggy Mountain Breakdown-07-04-2026",
 	"Misc Covers-You'Ve Gotta Move-07-04-2026",
 }
