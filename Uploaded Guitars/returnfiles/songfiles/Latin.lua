5a1c45507699a15ad11f10afccff76c15b2a40de
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 06:59:51 +0800

    Update Latin index: add "Desperado - Antonio Banderas - Cancion Del Mariachi" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Latin.lua
+++ Uploaded Guitars/returnfiles/songfiles/Latin.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Desperado - Antonio Banderas - Cancion Del Mariachi",
 	"Desvelado By Bobby Pulido",
 	"La Resaka By Supermerk2",
 	"Misc Covers-Beretta By El De Las R'S.-07-04-2026",
