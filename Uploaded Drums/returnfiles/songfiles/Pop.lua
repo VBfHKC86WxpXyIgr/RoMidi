0b624d00eaa69d03fd93116ea5ff835dd7154f65
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 20:01:33 +0800

    Update Pop index: add "Canon-In-D-Piano-Drums-Jpachelbel-Ftomatheus (2)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Pop.lua
+++ Uploaded Drums/returnfiles/songfiles/Pop.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Blinding Lights - The Weeknd",
+	"Canon-In-D-Piano-Drums-Jpachelbel-Ftomatheus (2)",
 	"Chamber Of Reflection - Mac DeMarco-Drum Kit, Set De Batterie",
 	"europop drumplosion",
 	"Mac DeMarco - Chamber of Reflection",
