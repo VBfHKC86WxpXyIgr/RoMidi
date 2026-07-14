13ada740a702e19e49c0e25629d6f7ee1b86f6aa
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-14 11:52:10 +0800

    Update Others index: add "1Boom, Boom, Boom, Boom!! - (HardTekk - The Dark Triad)" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -8,6 +8,7 @@ return {
 	"04 - Atic - MONTAGEM CEINTA (Nightcore)",
 	"072019",
 	"1 CộNg 1 LớN HơN 2",
+	"1Boom, Boom, Boom, Boom!! - (HardTekk - The Dark Triad)",
 	"1Dance With STEEL BALL RUN - Yugo Kanno (Piano)",
 	"1Fukashigi No Carte",
 	"1Hava Nagila",
