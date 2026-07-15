e513008494c20cd7f00734bc14576e35e1949f56
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-16 01:07:56 +0800

    Update Classical index: add "1Crucified - Army Of Lovers" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classical.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classical.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"1Crucified - Army Of Lovers",
 	"1moonlight Sonata 3rd Movement",
 	"1The Godfather Love Theme Piano Arrangement",
 	"Beethoven - Moonlight Sonata (3) [MIDIfind.Com]",
