f85e8947603529ad3481ac762f2161d1a1486fd1
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-15 11:43:48 +0800

    Update Rock index: add "Radiohead - Motion Picture Soundtrack [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -100,6 +100,7 @@ return {
 	"Radiohead - Just [MIDIfind.Com]",
 	"Radiohead - Kid A [MIDIfind.Com]",
 	"Radiohead - Lucky [MIDIfind.Com]",
+	"Radiohead - Motion Picture Soundtrack [MIDIfind.Com]",
 	"Radiohead - No Surprises [MIDIfind.Com]",
 	"Radiohead - Paranoid Android [MIDIfind.Com]",
 	"Radiohead - Subterranean Homesick Alien [MIDIfind.Com]",
