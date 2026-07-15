3df82e19bf8699ff6117d916f4b6fd43db2a8d19
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-16 01:07:03 +0800

    Update Classical index: add "1The Godfather Love Theme Piano Arrangement" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Classical.lua
+++ Uploaded Guitars/returnfiles/songfiles/Classical.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"1moonlight Sonata 3rd Movement",
+	"1The Godfather Love Theme Piano Arrangement",
 	"Beethoven - Moonlight Sonata (3) [MIDIfind.Com]",
 	"FrankSinatra My Way",
 	"Someone Like You (Easy Piano)",
