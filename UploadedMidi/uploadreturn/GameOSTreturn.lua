90e2d7097a66fe3475ab7319320578aeb3b27f59
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-18 07:16:37 +0800

    Update GameOST index: add "FNAF" [piano] via uploader

--- UploadedMidi/uploadreturn/GameOSTreturn.lua
+++ UploadedMidi/uploadreturn/GameOSTreturn.lua
@@ -48,6 +48,7 @@ return {
 	"Finale - Toby Fox",
 	"finale-undertale",
 	"Five nights at Freddy's Song",
+	"FNAF",
 	"Gaster's Theme - Toby Fox",
 	"Geometry Dash - Thinking Space II Nuke Powder",
 	"Geometry Dash - Time Machine",
