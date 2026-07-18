9fe6a5a84bb103db62b7532f6e9009320438c0c1
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-18 23:59:41 +0800

    Update Metal index: add "Rage Against The Machine - Bulls On Parade (2) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -260,6 +260,7 @@ return {
 	"Psychonaut 4 - World Of Pain And Hate [MIDIfind.Com]",
 	"Racer X - Scarified (2) [MIDIfind.Com]",
 	"Racer X - Technical Difficulties (3) [MIDIfind.Com]",
+	"Rage Against The Machine - Bulls On Parade (2) [MIDIfind.Com]",
 	"Rage Against The Machine - Killing In The Name (2) [MIDIfind.Com]",
 	"Rage Against The Machine - Killing In The Name [MIDIfind.Com]",
 	"RainingBlood",
