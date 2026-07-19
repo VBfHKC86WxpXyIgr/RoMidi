b048c4603ca0a9a1e3dc394a58bf20b334bcbab5
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-19 08:04:37 +0800

    Update Rock index: add "Stone Temple Pilots - Creep [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -123,6 +123,7 @@ return {
 	"Soda Stereo - De Musica Ligera [MIDIfind.Com]",
 	"Soda Stereo - Tratame Suavemente [MIDIfind.Com]",
 	"Still-Loving-You",
+	"Stone Temple Pilots - Creep [MIDIfind.Com]",
 	"Stone Temple Pilots - Plush [MIDIfind.Com]",
 	"Strokes (The) - Last Nite [MIDIfind.Com]",
 	"Strokes (The) - The End Has No End (7) [MIDIfind.Com]",
