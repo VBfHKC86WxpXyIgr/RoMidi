e618c4debd1b2736a88c988e24ea7b92b6cfd01d
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-20 01:09:31 +0800

    Update Modern index: add "1Oh Que Será - Willie Colon" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -13,6 +13,7 @@ return {
 	"1Laputa By Panchiko",
 	"1Ma Cherie Guitar By Malice Mizer",
 	"1Main Serong By The Changcuters",
+	"1Oh Que Será - Willie Colon",
 	"1Reunion By Korea Girl",
 	"2Pac - Changes",
 	"2Pac - Time Back - Piano Tutorial",
