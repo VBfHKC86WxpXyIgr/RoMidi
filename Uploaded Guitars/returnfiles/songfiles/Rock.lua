a9d1ddde935635215712a8b512d7562118e4f072
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-19 08:10:29 +0800

    Update Rock index: add "Soundgarden - Spoonman [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -122,6 +122,7 @@ return {
 	"Snow (Hey Oh!)",
 	"Soda Stereo - De Musica Ligera [MIDIfind.Com]",
 	"Soda Stereo - Tratame Suavemente [MIDIfind.Com]",
+	"Soundgarden - Spoonman [MIDIfind.Com]",
 	"Still-Loving-You",
 	"Stone Temple Pilots - Creep (3) [MIDIfind.Com]",
 	"Stone Temple Pilots - Creep [MIDIfind.Com]",
