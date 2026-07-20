519d2092b84da4f825491285c5f7459f691872e3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-20 23:56:54 +0800

    Update Rock index: add "Nirvana - Mexican Seafood [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -74,6 +74,7 @@ return {
 	"Nirvana - In Bloom [MIDIfind.Com]",
 	"Nirvana - Lithium (3) [MIDIfind.Com]",
 	"Nirvana - Lounge Act [MIDIfind.Com]",
+	"Nirvana - Mexican Seafood [MIDIfind.Com]",
 	"Nirvana - Milk It [MIDIfind.Com]",
 	"Nirvana - Nirvana Lithium Live MTV Vma 1992 [MIDIfind.Com]",
 	"Nirvana - Rape Me [MIDIfind.Com]",
