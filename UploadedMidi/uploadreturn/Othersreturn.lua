ded92212da56a34cfcfb6a8a96565609dfde64a4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-21 00:47:01 +0800

    Update Others index: add "1Old Doll (From Mad Father) - Bluerra-Sai (Piano)" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -20,6 +20,7 @@ return {
 	"1Is It Really You - Sleep Token, Loathe",
 	"1My September",
 	"1No Im Not A Human - Day Theme",
+	"1Old Doll (From Mad Father) - Bluerra-Sai (Piano)",
 	"1On The Square (Piano)",
 	"1Stay With Me - Miki Matsubara",
 	"1The Housebuilding Song",
