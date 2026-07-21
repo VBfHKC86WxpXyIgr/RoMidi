e7187919acee8f291c48410cb944d4c7b1a1d633
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-21 21:48:07 +0800

    Update Acoustic index: add "น้ำลาย (Num Lye) - Silly Fools [Fingerstyle] (1)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -66,6 +66,7 @@ return {
 	"ตาสว่าง - โมเดิร์นด็อก [Fingerstyle]",
 	"เธอไม่ได้สอนให้ฉันอยู่คนเดียว - SHERRY",
 	"น้ำลาย (Num Lye) - Silly Fools [Fingerstyle]",
+	"น้ำลาย (Num Lye) - Silly Fools [Fingerstyle] (1)",
 	"ผิดที่ไว้ใจ Silly Fools",
 	"พิจารณา (Consider) - Musketeers Ft Maiyarap",
 	"ไม่บอกเธอ - Bedroom Audio",
