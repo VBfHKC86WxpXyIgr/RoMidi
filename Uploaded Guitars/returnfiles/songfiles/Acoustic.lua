ef5560e352cb4d376af6c4ab16b67b45a643867a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-22 21:03:07 +0800

    Update Acoustic index: add "PT Adamczyk Olga Jankowska Cyberpunk 2077 - Never Fade Away SAMURAI Cover Guitar Solo" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -54,6 +54,7 @@ return {
 	"Oasis - Married With Children",
 	"Paradox - Official MV",
 	"Paradox - Official MV-Drum Kit (1)",
+	"PT Adamczyk Olga Jankowska Cyberpunk 2077 - Never Fade Away SAMURAI Cover Guitar Solo",
 	"Radiohead - No Surprises",
 	"Ring-Around-The-Rosie",
 	"Roundabout- Bass Only",
