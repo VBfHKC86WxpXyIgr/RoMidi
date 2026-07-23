a09685765b58f23f4c1909f4a655638d41892791
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-23 23:32:12 +0800

    Update Rock index: add "ACDC.Highway To Hell K" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -2,6 +2,7 @@
 return {
 	"21 Guns - Green Day (Piano-Vocal-Guitar)",
 	"ACDC - Thunderstruck [MIDIfind.Com]",
+	"ACDC.Highway To Hell K",
 	"Alice In Chains - Damm That River [MIDIfind.Com]",
 	"Alice In Chains - It Ain'T Like It [MIDIfind.Com]",
 	"Alice In Chains - Man In A Box",
