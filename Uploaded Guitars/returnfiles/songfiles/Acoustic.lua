afc2b24e147748bd777917dc9df111c0cdd4bf8a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-23 10:01:39 +0800

    Update Acoustic index: add "Smash Mouth - All Star" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -66,6 +66,7 @@ return {
 	"Silly Fools - - Silly Fools Lyrics Audio",
 	"Silly Fools - Kid Hord(1)",
 	"Slot Machine -",
+	"Smash Mouth - All Star",
 	"Smash Mouth - Walkin On The Sun",
 	"Smashing Pumpkins - Luna",
 	"Smile Buffalo -",
