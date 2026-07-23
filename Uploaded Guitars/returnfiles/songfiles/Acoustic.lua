ab0502a7dbbaddd010916c7d7f480a932a01b8ee
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-23 09:58:36 +0800

    Update Acoustic index: add "The Beatles - Here Comes The Sun" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -73,6 +73,7 @@ return {
 	"Sunroof - Nicky Youre, Dazy",
 	"Tarot Feast 1780242551896",
 	"The Beatles - Dont Let Me Down",
+	"The Beatles - Here Comes The Sun",
 	"The Beatles - Let It Be",
 	"Three Man Down - ถ้าเธอรักฉันจริง",
 	"Tip Toe - HYBS",
