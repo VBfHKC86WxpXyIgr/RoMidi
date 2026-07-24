1b4bcf1d3f201ca9ffaa0e0321efc8b51182ab5d
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-24 22:49:19 +0800

    Update Flamenco index: add "Maki VS Naoya Full Version-Jujutsu Kaisen S3 EP4 OST Cover - The Culling Game - Zenin Clan Fight-02-28-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Flamenco.lua
+++ Uploaded Guitars/returnfiles/songfiles/Flamenco.lua
@@ -1,1 +1,4 @@
-return {}
+-- Auto-updated by the Cloudflare Worker on upload/delete
+return {
+	"Maki VS Naoya Full Version-Jujutsu Kaisen S3 EP4 OST Cover - The Culling Game - Zenin Clan Fight-02-28-2026",
+}
