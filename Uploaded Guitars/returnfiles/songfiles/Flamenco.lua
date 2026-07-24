6c8375fce95cbc6bbff778b554a36c90804d240c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-24 22:51:00 +0800

    Update Flamenco index: add "Defeat Here (Jujutsu Kaisen S3 OST) - Yoshimasa Terui" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Flamenco.lua
+++ Uploaded Guitars/returnfiles/songfiles/Flamenco.lua
@@ -1,4 +1,5 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Defeat Here (Jujutsu Kaisen S3 OST) - Yoshimasa Terui",
 	"Maki VS Naoya Full Version-Jujutsu Kaisen S3 EP4 OST Cover - The Culling Game - Zenin Clan Fight-02-28-2026",
 }
