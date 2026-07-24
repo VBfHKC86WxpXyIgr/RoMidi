1f15d7de03e5a6166d68b1c427e180d1275af49c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-24 22:44:06 +0800

    Update Latin index: add "Tristeza Y TraicióN-Danny Lux-06-29-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Latin.lua
+++ Uploaded Guitars/returnfiles/songfiles/Latin.lua
@@ -5,4 +5,5 @@ return {
 	"La Resaka By Supermerk2",
 	"Misc Covers-Beretta By El De Las R'S.-07-04-2026",
 	"Misc Covers-La Cheyenne By El De Las R’S-07-04-2026",
+	"Tristeza Y TraicióN-Danny Lux-06-29-2026",
 }
