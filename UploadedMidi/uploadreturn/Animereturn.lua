73bd0ff7d71c955d99c311324e3b0da9d4e84a7b
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-24 23:13:21 +0800

    Update Anime index: add "A Cruel Angel'S Thesis - Neon Genesis Evangelion OP" [piano] via uploader

--- UploadedMidi/uploadreturn/Animereturn.lua
+++ UploadedMidi/uploadreturn/Animereturn.lua
@@ -8,6 +8,7 @@ return {
 	"1Jotaro'S Theme From Jojo'S Bizarre Adventures Stardust Crusaders",
 	"1Red Swan - Attack On Titan Season 3 Opening",
 	"200 AniSong Medley HalcyonMusic",
+	"A Cruel Angel'S Thesis - Neon Genesis Evangelion OP",
 	"Ai ĐưA Em Về Take Me Back Home",
 	"Ai No Uta",
 	"AIZO HalcyonMusic",
