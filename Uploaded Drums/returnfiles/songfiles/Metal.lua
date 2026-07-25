268b555de5fa7261f1d8d3e285a58bc2d6ef11a7
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-26 03:11:57 +0800

    Update Metal index: add "Metallica - Seek And Destroy (Drum)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -190,6 +190,7 @@ return {
 	"Metallica - One [MIDIfind.Com]",
 	"Metallica - Phantom Lord [MIDIfind.Com]",
 	"Metallica - Sad But True (2) [MIDIfind.Com]",
+	"Metallica - Seek And Destroy (Drum)",
 	"Metallica - Seek And Destroy [MIDIfind.Com]",
 	"Metallica - So What (2) [MIDIfind.Com]",
 	"Metallica - Some Kind Of Monster [MIDIfind.Com]",
