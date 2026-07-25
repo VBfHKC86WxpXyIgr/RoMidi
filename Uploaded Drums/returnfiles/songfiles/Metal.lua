b46336a4f664377be23e40701860f7d56f43f175
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-26 02:27:58 +0800

    Update Metal index: add "Slayer - Seasons In The Abyss (Drum)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -267,6 +267,7 @@ return {
 	"Slapshock - Direction [MIDIfind.com]",
 	"Slayer - Angel Of Death (2) [MIDIfind.Com]",
 	"Slayer - Raining Blood [MIDIfind.Com]",
+	"Slayer - Seasons In The Abyss (Drum)",
 	"Slipknot - Before I Forget.drum midi",
 	"Slipknot - Dead Memories (5) [MIDIfind.com]",
 	"Slipknot - Eyeless",
