a8558c2a462ec951eee0bee01409afad5dfa403e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-26 02:52:35 +0800

    Update Funk index: add "Red Hot Chili Peppers - Under The Bridge (Drum)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Funk.lua
+++ Uploaded Drums/returnfiles/songfiles/Funk.lua
@@ -1,4 +1,5 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Daft-Punk-Ft-Pharell-Get-Lucky-Samcera-20201004170621-Nonstop2k.Com",
+	"Red Hot Chili Peppers - Under The Bridge (Drum)",
 }
