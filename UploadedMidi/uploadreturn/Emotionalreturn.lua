56813df2621fbecda92f459a2c5efcab5aa5f96c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-26 00:43:02 +0800

    Update Emotional index: add "1Bad - Wave To Earth (Lead Sheet)" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -3,6 +3,7 @@ return {
 	"(Play!) The Neighbourhood-Sweater Weather",
 	"[Black MIDI] The Classic Black Medley",
 	"「 ꕤ 」 S0rrow - Unhappy",
+	"1Bad - Wave To Earth (Lead Sheet)",
 	"1Break By Alex G",
 	"1Innocence - Daniel Caesar (Piano)",
 	"1Join Me In Death - HIM (Piano Cover)",
