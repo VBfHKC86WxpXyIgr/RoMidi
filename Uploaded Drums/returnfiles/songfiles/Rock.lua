3fa9137e5d5ccdc779725715b8ffd1fc4f2e65b2
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-26 03:53:16 +0800

    Update Rock index: add "My Chemical Romance - Welcome To The Black Parade (Drum)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Rock.lua
+++ Uploaded Drums/returnfiles/songfiles/Rock.lua
@@ -99,6 +99,7 @@ return {
 	"Mac Demarco - Freaking Out The Neighborhood [MIDIfind.Com]",
 	"My Chemical Romance - I'M Not Okay I Promise [MIDIfind.Com]",
 	"My Chemical Romance - Thank You For The Venom [MIDIfind.Com]",
+	"My Chemical Romance - Welcome To The Black Parade (Drum)",
 	"My Chemical Romance - You Know What They Do To Guys Like Us In Prison (2) [MIDIfind.Com]",
 	"Nirvana - All Apologies (2) [MIDIfind.Com]",
 	"Nirvana - Aneurysm [MIDIfind.Com]",
