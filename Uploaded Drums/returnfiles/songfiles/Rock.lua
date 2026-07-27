10ac108de6686d11005bf93cb4aec1144fcc6c3e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-27 17:14:37 +0800

    Update Rock index: add "ACDC - Thats The Way I Wanna Rock'N'Roll [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Rock.lua
+++ Uploaded Drums/returnfiles/songfiles/Rock.lua
@@ -3,6 +3,7 @@ return {
 	"21 Guns - Green Day (Green Day) 21 Guns",
 	"4 Non Blondes - What'S Up (Drum)",
 	"505",
+	"ACDC - Thats The Way I Wanna Rock'N'Roll [MIDIfind.Com]",
 	"ACDC - Thunderstruck (Drum)",
 	"ACDC - You Shook Me All Night Long (Drum)",
 	"Alice In Chains - Damm That River [MIDIfind.Com]",
