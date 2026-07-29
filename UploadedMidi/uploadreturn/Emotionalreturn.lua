3e188d54a6c9aca42dab5889f112eaf9d7d3becf
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-30 00:25:59 +0800

    Update Emotional index: add "Arrival Of The Birds The Cinematic Orchestra (BEST VERSION)" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -37,6 +37,7 @@ return {
 	"Aria-Math-C418-Arr-By-Chris-Fairmanminecraft-Creative(BEST VERSION)",
 	"Ariana Grande - Hampstead - Accurate Piano Tutorial With Sheet Music",
 	"Ariana Grande & Justin Bieber - Stuck With U (Midi By Carlo Prato) (Www.Cprato.Com)",
+	"Arrival Of The Birds The Cinematic Orchestra (BEST VERSION)",
 	"As The World Caves In - Matt Maltese (1)",
 	"Avenged Sevenfold - M I A [MIDIfind.com]",
 	"Avenged Sevenfold - M.i.a.   WWW.MIDISFREE.COM",
