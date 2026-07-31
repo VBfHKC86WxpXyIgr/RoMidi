201f822a6539e6c1ec9939612aed738f630fd0a5
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-01 00:08:02 +0800

    Update Folk index: add "Knockin'On Heaven'S Door - Bob Dylan" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Folk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Folk.lua
@@ -5,6 +5,7 @@ return {
 	"Blowin' In The Wind - Bob Dylan, Peter, Paul & Mary (Piano-Vocal-Guitar)",
 	"Bob Dylan - Like A Rolling Stone",
 	"House-Of-The-Rising-Sun-1.Mid",
+	"Knockin'On Heaven'S Door - Bob Dylan",
 	"Like A Rolling Stone - Bob Dylan (Piano-Vocal-Guitar)",
 	"Misc Covers-Foggy Mountain Breakdown-07-04-2026",
 	"Misc Covers-You'Ve Gotta Move-07-04-2026",
