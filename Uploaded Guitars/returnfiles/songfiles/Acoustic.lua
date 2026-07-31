0b81eca3f46bebf4fd269fbf3694e10dbd426c02
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-07-31 15:58:33 +0800

    Update Acoustic index: add "Deftones - Be Quiet And Drive Far AwayBass" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -35,6 +35,7 @@ return {
 	"Composed By Yu-Peng Chen Arranged By Devon Sebastian K - Liyue Harbor Song No. 4",
 	"Creed - One Last Breath",
 	"Deep Purple - Smoke On The Water-Drum Kit",
+	"Deftones - Be Quiet And Drive Far AwayBass",
 	"Dire Straits - Sultans Of Swing",
 	"Dorian Concept - 'Hide",
 	"Dumb Ways To Die",
