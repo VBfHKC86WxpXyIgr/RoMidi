4178fd4088b80488ae6332775ce2dc332237e7b3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-01 00:04:19 +0800

    Update Folk index: add "Bob Dylan - Like A Rolling Stone" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Folk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Folk.lua
@@ -2,6 +2,7 @@
 return {
 	"1Hava Nagila",
 	"1The Housebuilding Song",
+	"Bob Dylan - Like A Rolling Stone",
 	"House-Of-The-Rising-Sun-1.Mid",
 	"Misc Covers-Foggy Mountain Breakdown-07-04-2026",
 	"Misc Covers-You'Ve Gotta Move-07-04-2026",
