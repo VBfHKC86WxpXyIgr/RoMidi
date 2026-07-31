7f4f75449a102bc65cc03686c9f16f4ec7d12ec2
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-01 00:08:11 +0800

    Update Folk index: add "Like A Rolling Stone - Bob Dylan" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Folk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Folk.lua
@@ -6,6 +6,7 @@ return {
 	"Bob Dylan - Like A Rolling Stone",
 	"House-Of-The-Rising-Sun-1.Mid",
 	"Knockin'On Heaven'S Door - Bob Dylan",
+	"Like A Rolling Stone - Bob Dylan",
 	"Like A Rolling Stone - Bob Dylan (Piano-Vocal-Guitar)",
 	"Misc Covers-Foggy Mountain Breakdown-07-04-2026",
 	"Misc Covers-You'Ve Gotta Move-07-04-2026",
