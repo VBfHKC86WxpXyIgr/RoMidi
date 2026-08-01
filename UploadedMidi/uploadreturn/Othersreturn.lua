5a36983e77fe504a1d38af9f79984878c660bd93
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-01 10:17:04 +0800

    Update Others index: add "CLOWNOFOBIA🤡🔪" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -129,6 +129,7 @@ return {
 	"ChúNg Ta KhôNg ThuộC Về Nhau",
 	"ChúNg Ta KhôNg ThuộC Về Nhau 2",
 	"ChuyệN ĐôI Ta X VếT MưA",
+	"CLOWNOFOBIA🤡🔪",
 	"Có ChắC YêU Là đâY",
 	"Có ChàNg Trai ViếT LêN CâY",
 	"Có ChúT NgọT NgàO",
