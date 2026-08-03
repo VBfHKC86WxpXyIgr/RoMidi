0c738487d60d079109a440c78971ad17f45928a3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-03 09:00:39 +0800

    Update Emotional index: add "Mystique(HUMAN BEST VERSION)" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -211,6 +211,7 @@ return {
 	"My Chemical Romance - I Don'T Love You",
 	"My Heart - Fikram",
 	"My-Love-Mine-All-Mine-Mitski-Arrangement(BEST VERSION)",
+	"Mystique(HUMAN BEST VERSION)",
 	"Như có Bác trong ngày Đại thắng impossible",
 	"Nightchanges by dc ; @jvkowi",
 	"No Surprised",
