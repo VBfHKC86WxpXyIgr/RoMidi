d9e0c0a6c8fdeb4dcff89a2931e1e1c26db7a0d9
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-04 22:24:50 +0800

    Update Others index: add "Cause I Love You 3" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -106,6 +106,7 @@ return {
 	"Carol Of The Bells X Moonlight Sonata 3rd X Golden Hour X Dieu Ne Ment Jamais X Hit The Road Jack X Back To Black X Czardas X Bohemian Rhapsody X River Flow In You X Experience X La Vie Qu’On MèNe X Rush E",
 	"Cats On Mars - The Seatbelts",
 	"Cause I Love You - Noo Phuoc Thinh Piano",
+	"Cause I Love You 3",
 	"Cause I Love You X GạT đI Nc MắT",
 	"Cause-I-Love-You-2",
 	"Celestial Opus By Ace",
