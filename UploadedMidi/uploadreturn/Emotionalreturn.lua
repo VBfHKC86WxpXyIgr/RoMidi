ed2be6454bb497ed9d8e082fc4e303b1f80545ee
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-06 08:11:20 +0800

    Update Emotional index: add "TIKTOK COMPILATION (😭🥀HUMAN BEST VERSION)" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -312,6 +312,7 @@ return {
 	"TheFatRat - Fly Away. Black MIDI.Mid",
 	"This Is What Autumn Feels Like - Jvke",
 	"Through-Patches(HUMAN BEST VERSION)",
+	"TIKTOK COMPILATION (😭🥀HUMAN BEST VERSION)",
 	"Time Travel Theme By Jay Chou From Secret Film (BEST VERSION)",
 	"Time Travel Theme Jay Chou Secret Ending (BEST Advanced Mode)",
 	"Tokyo Ghoul OP Unravel FB Piano Anime (BEST VERSION)",
