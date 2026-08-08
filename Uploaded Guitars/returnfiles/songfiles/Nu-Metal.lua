eb347a39d0c3b39604130fe775a92c7b5833c06c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-08 16:32:51 +0800

    Update Nu-Metal index: add "Static-X - I'M With Stupid [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
@@ -22,6 +22,7 @@ return {
 	"Metallica - So What [MIDIfind.Com]",
 	"Psychosocial - Slipknot",
 	"Slipknot - Eyeless (2) [MIDIfind.Com]",
+	"Static-X - I'M With Stupid [MIDIfind.Com]",
 	"Static-X - Push It (2) [MIDIfind.Com]",
 	"Sweet Child O' Mine (Solo) - Guns N Roses",
 	"System Of A Down - Aerials [MIDIfind.Com]",
