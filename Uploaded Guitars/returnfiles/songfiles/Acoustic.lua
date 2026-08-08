37047cfbb231ba56e43fb7f75bd0d6d9d1ed1622
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-08 22:08:02 +0800

    Update Acoustic index: add "Pirates Of The Caribbean - He'S A Pirate (3)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -88,6 +88,7 @@ return {
 	"Oasis - Married With Children",
 	"Paradox - Official MV",
 	"Paradox - Official MV-Drum Kit (1)",
+	"Pirates Of The Caribbean - He'S A Pirate (3)",
 	"Pixies - Where Is My Mind",
 	"Plastic Tree - Kuuchu Buranko-Electric Guitar",
 	"PT Adamczyk Olga Jankowska Cyberpunk 2077 - Never Fade Away SAMURAI Cover Guitar Solo",
