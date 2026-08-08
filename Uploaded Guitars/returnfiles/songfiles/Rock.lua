c6234f6837625787f38c09c127122951a7c3b031
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-09 03:44:17 +0800

    Update Rock index: add "Hendrix Jimi - All Along The Watchower [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -50,6 +50,7 @@ return {
 	"Free Bird - Lynyrd Skynyrd",
 	"Gorillaz - Feel Good Inc [MIDIfind.Com]",
 	"Heart Shaped Box - Nirvana",
+	"Hendrix Jimi - All Along The Watchower [MIDIfind.Com]",
 	"Hendrix Jimi - Foxey Lady [MIDIfind.Com]",
 	"Hendrix Jimi - The Star Spangled Banner [MIDIfind.Com]",
 	"Heroes Main",
