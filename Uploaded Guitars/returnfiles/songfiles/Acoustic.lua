34a9407b34c3ab5aa3c0586ec82a5ed06239ac04
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-10 08:01:25 +0800

    Update Acoustic index: add "Malcolm Todd - Roommates" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -81,6 +81,7 @@ return {
 	"Mac Demarco - Freaking Out The Neighborhood (1)",
 	"Made In Thailand, Made In USA",
 	"Malcolm Todd - Earrings For Band",
+	"Malcolm Todd - Roommates",
 	"Mayonnaise - Synestheisa [MIDIfind.Com]",
 	"Michael Jackson - Smooth Criminal Acoustic Fingerstyle",
 	"Misc Covers-Is It Really You (Acoustic VER.) By Loathe-07-04-2026",
