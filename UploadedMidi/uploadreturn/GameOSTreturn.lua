093e9c512e8133317387e0f3a20c5d3e5812446f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-11 06:27:03 +0800

    Update GameOST index: add "The Legend Of Zelda Ocarina Of Time - Great Fairy Fountain (1)" [piano] via uploader

--- UploadedMidi/uploadreturn/GameOSTreturn.lua
+++ UploadedMidi/uploadreturn/GameOSTreturn.lua
@@ -164,6 +164,7 @@ return {
 	"The Green Orbs - At the Fair (Piano Cover)",
 	"The Green Orbs - Claudio The Worm (Duet)",
 	"The Green Orbs - Old MacDonald Instrumental (Happy)",
+	"The Legend Of Zelda Ocarina Of Time - Great Fairy Fountain (1)",
 	"The Legend Of Zelda Ocarina Of Time - Lost Woods",
 	"The NOexistenceN Of You AND Me - Just You",
 	"The Nuker 4 Audio Remake (Original Composition From TN1.5)",
