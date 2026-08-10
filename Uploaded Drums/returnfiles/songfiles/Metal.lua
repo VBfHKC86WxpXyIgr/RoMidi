e32928b97b04358651f5d3d734655dbbb1078253
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-11 00:10:39 +0800

    Update Metal index: add "Drum San Sebastian Sonata Arctica" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -103,6 +103,7 @@ return {
 	"Drum - Drums Fill and Solos Patterns [MIDIfind.com]",
 	"Drum - Heavy Metal Drums [MIDIfind.com]",
 	"Drum - Playing on Hi Hat With Two Hands [MIDIfind.com]",
+	"Drum San Sebastian Sonata Arctica",
 	"Duality",
 	"Duality - Slipknot",
 	"Elena Siegman - 115",
