64b700ec6e6c41de9544d036c108c2ccfd8d4c5e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-12 10:49:21 +0800

    Update Indie index: add "Bass Teen Rebel The Haunted Youth Tuning Drop D" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Indie.lua
+++ Uploaded Guitars/returnfiles/songfiles/Indie.lua
@@ -4,6 +4,7 @@ return {
 	"1On The Square (Piano)",
 	"Angus And Julia Stone - Big Jet Plane [MIDIfind.Com]",
 	"Bass For Now Not Ever-Motifs",
+	"Bass Teen Rebel The Haunted Youth Tuning Drop D",
 	"Boys Don'T Cry - The Cure (Piano Arrangement)",
 	"Brubeck Dave - Take Five [MIDIfind.Com]",
 	"Creep - Radiohead (Guitar)",
