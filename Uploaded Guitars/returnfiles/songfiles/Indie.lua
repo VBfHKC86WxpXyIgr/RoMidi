6b519b577a58c135d2df8f616125ce1da23e51fd
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-12 08:17:23 +0800

    Update Indie index: add "Guitar For Now Not Ever-Motifs" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Indie.lua
+++ Uploaded Guitars/returnfiles/songfiles/Indie.lua
@@ -7,6 +7,7 @@ return {
 	"Boys Don'T Cry - The Cure (Piano Arrangement)",
 	"Brubeck Dave - Take Five [MIDIfind.Com]",
 	"Creep - Radiohead (Guitar)",
+	"Guitar For Now Not Ever-Motifs",
 	"Guns N' Roses - November Rain [MIDIfind.Com]",
 	"Led Zeppelin-Stairway To Heaven-06-11-2026 (1)",
 	"Mac Demarco - Freaking Out The Neighborhood [MIDIfind.Com]",
