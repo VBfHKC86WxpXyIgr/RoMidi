b5b5f5833fd292370715a74c6af01298c18fa0fc
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-13 00:10:17 +0800

    Update Others index: add "RUSH E MADE BY GROK" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -492,6 +492,7 @@ return {
 	"RUSH E 3 Whitout Anything Else",
 	"Rush E 4",
 	"Rush E 4 No Lag",
+	"RUSH E MADE BY GROK",
 	"Rush E Normal Mhxa",
 	"Rush E X Beethoven Viruss X Nu Pogodi",
 	"Rush E X Interstellar",
