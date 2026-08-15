e5679c28f2365d04d730b3af625663cab04c7d06
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-15 12:49:44 +0800

    Update Indie index: add "Guitar Lead Surf Curse Freaks" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Indie.lua
+++ Uploaded Guitars/returnfiles/songfiles/Indie.lua
@@ -13,6 +13,7 @@ return {
 	"Creep - Radiohead (Guitar)",
 	"Guitar Alicia Viajes En El Tiempo",
 	"Guitar For Now Not Ever-Motifs",
+	"Guitar Lead Surf Curse Freaks",
 	"Guitar Rtm Surf Curse Freaks",
 	"Guitar1 Alv Bye Melted Ice Cream",
 	"Guitar1 Eidolon Hibou",
