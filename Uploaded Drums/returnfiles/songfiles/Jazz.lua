55a7c610e8e361b81f467b9ea207eeb839461244
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-20 20:57:23 +0800

    Update Jazz index: add "A Night In Tunisia Drum Part" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Jazz.lua
+++ Uploaded Drums/returnfiles/songfiles/Jazz.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"A Night In Tunisia Drum Part",
 	"A.JACKSON.Livin On Love",
 	"B.B King - The Thrill Is Gone [MIDIfind.Com]",
 	"Caravan(1)",
