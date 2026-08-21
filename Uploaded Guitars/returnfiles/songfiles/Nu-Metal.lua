34ba9a31d99f454b360b31da1eebc915d286aa80
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-21 20:47:17 +0800

    Update Nu-Metal index: add "Deftones - Knife Party (2) [MIDIfind.Com]" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Nu-Metal.lua
@@ -4,6 +4,7 @@ return {
 	"Deftones - Be Quiet And Drive [MIDIfind.Com]",
 	"Deftones - Change In The House Of Flies [MIDIfind.Com]",
 	"Deftones - Engine Number (9) [MIDIfind.Com]",
+	"Deftones - Knife Party (2) [MIDIfind.Com]",
 	"Deftones - Knife Prty-06-24-2026",
 	"Deftones - My Own Summer (Shove It)",
 	"Deftones - My Own Summer Shove It [MIDIfind.Com]",
