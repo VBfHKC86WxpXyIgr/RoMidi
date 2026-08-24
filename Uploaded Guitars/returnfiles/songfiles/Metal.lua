8cc01862a56d27f5e8797d18fc7ee150b54be97c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-25 03:05:51 +0800

    Update Metal index: add "Spit Out The Bone - Metallica" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -323,6 +323,7 @@ return {
 	"Slipknot - Sic [MIDIfind.Com]",
 	"Slipknot - The Heretic Anthem (4) [MIDIfind.Com]",
 	"Smoke On The Water - Deep Purple Smoke On The Water - Deep Purple",
+	"Spit Out The Bone - Metallica",
 	"Stabwound - Necrophagist Piano Arrangement (Sorta) Lyrics!!",
 	"Symphony X - Sea Of Lies First Part",
 	"System Of A Down - B.Y.O.B. [MIDIfind.Com]",
