3d700fd0a513ae1827fb701e492a19ba952c216f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-25 16:15:47 +0800

    Update Modern index: add "Poster Boy - 2hollis" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -183,6 +183,7 @@ return {
 	"Pasilyo - Sun Kissed Lola (Piano Cover)",
 	"Pasilyo - Sunkissed Lola (Piano)",
 	"Phonecert (폰서트) - 10CM",
+	"Poster Boy - 2hollis",
 	"PUN - DAY ONE Piano Tutorial",
 	"Quando Bate Aquela Saudade - Rubel Piano",
 	"rapp snitch knishes",
