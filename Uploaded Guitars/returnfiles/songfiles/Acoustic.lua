fd3d417712ca8a8e36ed5ed95a4bd9f0df442784
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-26 10:49:41 +0800

    Update Acoustic index: add "CLAIRE - FLY ME TO THE MOON Neon Genesis Evangelion" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -46,6 +46,7 @@ return {
 	"Chris Isaakwicked Game",
 	"Cinderella - Tattoo Colour",
 	"Clair-De-Lune-Debussy",
+	"CLAIRE - FLY ME TO THE MOON Neon Genesis Evangelion",
 	"Clarion - Hello Juliet",
 	"Come As You Are - Nirvana (Guitar Tab)",
 	"Composed By Yu-Peng Chen Arranged By Devon Sebastian K - Liyue Harbor Song No. 4",
