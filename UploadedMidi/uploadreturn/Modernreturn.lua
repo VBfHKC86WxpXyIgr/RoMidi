741be2c0035e7d62acc43e6378c8f8810413eb7c
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-29 01:45:49 +0800

    Update Modern index: add "Beethoven Virus But It'S Insane (MS)" [piano] via uploader

--- UploadedMidi/uploadreturn/Modernreturn.lua
+++ UploadedMidi/uploadreturn/Modernreturn.lua
@@ -51,6 +51,7 @@ return {
 	"basic pitch transcription",
 	"Beanie",
 	"Beatles Let It Be",
+	"Beethoven Virus But It'S Insane (MS)",
 	"Beggin' – Måneskin Drum Sheet Music",
 	"Blood Theme (From Dexter) - Daniel J Licht (Piano Solo)",
 	"blue - yung kai",
