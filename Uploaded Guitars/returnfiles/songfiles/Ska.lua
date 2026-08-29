5a75aeefb62ea6300db86244896c1c0cdb588be4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-30 06:21:46 +0800

    Update Ska index: add "1Stranded Lullaby (Piano)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Ska.lua
+++ Uploaded Guitars/returnfiles/songfiles/Ska.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"1Stranded Lullaby (Piano)",
 	"1White Ball - Miracle Musical (ミラクルミュージカル) White Ball",
 	"AC DC - Thunderstruck - Opening Guitar Solo",
 	"As Time Flies - Tyrique Sayre (Piano Solo)",
