c575961526f78b414493871e8a378c59f9353c12
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-30 06:19:30 +0800

    Update Ska index: add "Baggy Trousers - Madness (Piano-Voice-Guitar)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Ska.lua
+++ Uploaded Guitars/returnfiles/songfiles/Ska.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
+	"Baggy Trousers - Madness (Piano-Voice-Guitar)",
 	"Bass Little Girls OB",
 	"Guitar Lead Little Girls OB",
 	"Guitar Rtm Little Girls OB",
