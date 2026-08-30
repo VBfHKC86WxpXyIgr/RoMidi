2f77aca4635ac824b34a807660ca417809b5f092
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-30 12:13:10 +0800

    Update Emotional index: add "Nana - Endless Story" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -247,6 +247,7 @@ return {
 	"My Heart - Fikram",
 	"My-Love-Mine-All-Mine-Mitski-Arrangement(BEST VERSION)",
 	"Mystique(HUMAN BEST VERSION)",
+	"Nana - Endless Story",
 	"Như có Bác trong ngày Đại thắng impossible",
 	"Nightchanges by dc ; @jvkowi",
 	"No Surprised",
