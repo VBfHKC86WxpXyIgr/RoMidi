dfd84af9546a5c6936a45f0c5298625e93220107
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-30 12:14:40 +0800

    Update Emotional index: add "Nana - Rose" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -248,6 +248,7 @@ return {
 	"My-Love-Mine-All-Mine-Mitski-Arrangement(BEST VERSION)",
 	"Mystique(HUMAN BEST VERSION)",
 	"Nana - Endless Story",
+	"Nana - Rose",
 	"Nana - Yearning",
 	"NANA-M",
 	"Như có Bác trong ngày Đại thắng impossible",
