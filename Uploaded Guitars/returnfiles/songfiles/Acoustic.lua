4075ed95a63e38eabbb877189e3897834d4c4199
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-08-31 08:20:01 +0800

    Update Acoustic index: add "ACDC - Thunderstruck" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -11,6 +11,7 @@ return {
 	"62977 GITARA",
 	"About You By The 1780242398856",
 	"ACDC - Dirty Deeds Done Dirt Cheap",
+	"ACDC - Thunderstruck",
 	"Acrophobia - Penguin Villa",
 	"American Football - Never Meant (1)",
 	"Andra And The BackBone - Sempurna Lyre.Mid",
