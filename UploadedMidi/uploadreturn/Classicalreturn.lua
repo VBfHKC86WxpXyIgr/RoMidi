7d98a98df3ba27c54cd3fabaac5e3f42601bc58a
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-02 03:50:06 +0800

    Update Classical index: add "Libet'S Delay" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -160,6 +160,7 @@ return {
 	"Lana Del Rey - White Mustang Lyrics Mp3cutnet Converted By Jukeblocks",
 	"Lana Del Rey-Diet Mountain Dew The Flight - SafeShare - Screencastify - January 5 2026 9 34 PM Trimmed By Jukeblocks Converted By Jukeblocks",
 	"Libertango(Piano Solo) - Astor Piazzolla",
+	"Libet'S Delay",
 	"liebestraum",
 	"Liebestraum-S-541-No-3-In-A-Major-Liszt(BEST VERSION)",
 	"Liquid Smooth - Mitski",
