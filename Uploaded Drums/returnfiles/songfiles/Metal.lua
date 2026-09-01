6afc934d072cd41fd5ea4063df50d1610faaac4d
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-01 17:44:12 +0800

    Update Metal index: add "Metallica - The Thing That Should Not Be (3) [MIDIfind.Com]" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -228,6 +228,7 @@ return {
 	"Metallica - The Call Of Ktulu [MIDIfind.Com]",
 	"Metallica - The Four Horseman [MIDIfind.Com]",
 	"Metallica - The Struggle Within [MIDIfind.Com]",
+	"Metallica - The Thing That Should Not Be (3) [MIDIfind.Com]",
 	"Metallica - The Unforgiven [MIDIfind.Com]",
 	"Metallica - To Live Is To Die (11) [MIDIfind.Com]",
 	"Metallica - Trapped Under Ice [MIDIfind.Com]",
