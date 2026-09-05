92ff5ef89457ce930a30d6a4eb852ab59b9104f4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-05 09:39:18 +0800

    Update Acoustic index: add "Don'T Fear The Reaper Guitar1-Greed" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -69,6 +69,7 @@ return {
 	"Do Re Mi (Teste1)",
 	"Docter Dance",
 	"Don'T Fear The Reaper Bass-Greed",
+	"Don'T Fear The Reaper Guitar1-Greed",
 	"Don'T Fear The Reaper Guitar2-Greed",
 	"Don'T Fear The Reaper GuitarRTM-Greed",
 	"Dorian Concept - 'Hide",
