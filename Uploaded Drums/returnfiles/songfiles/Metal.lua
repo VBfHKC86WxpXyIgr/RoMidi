89f6ec88dfedf633cd9a9c3f0aa5d779906212f8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-07 09:58:21 +0800

    Update Metal index: add "Megadeth - Holy Wars The Punishment Due (Drum Updated)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -183,6 +183,7 @@ return {
 	"Megadeth - Devils Island [MIDIfind.Com]",
 	"Megadeth - Holy Wars",
 	"Megadeth - Holy Wars The Punishment (Drum)",
+	"Megadeth - Holy Wars The Punishment Due (Drum Updated)",
 	"Megadeth - Holy Wars The Punishment Due [MIDIfind.Com]",
 	"Megadeth - Peace Sells [MIDIfind.Com]",
 	"Megadeth - Symphony Of Destruction [MIDIfind.Com]",
