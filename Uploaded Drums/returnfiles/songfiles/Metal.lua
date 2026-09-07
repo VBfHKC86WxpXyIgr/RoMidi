249e923f50276a4c32e6c8f04c38bc4b106bfb8e
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-07 10:03:10 +0800

    Update Metal index: add "Sepultura - Attitude (Drum)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Metal.lua
+++ Uploaded Drums/returnfiles/songfiles/Metal.lua
@@ -310,6 +310,7 @@ return {
 	"Scorpions - Rock You Like A Hurricane [MIDIfind.Com]",
 	"Scream",
 	"Sepultura - Arise (Drum)",
+	"Sepultura - Attitude (Drum)",
 	"Sepultura - Ratamahatta (Drum)",
 	"Sepultura - Refuse Resist (Drum)",
 	"Sepultura - Roots Bloody Roots (3) [MIDIfind.Com]",
