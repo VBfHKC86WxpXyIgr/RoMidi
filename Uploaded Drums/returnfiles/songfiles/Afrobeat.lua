40266a3fc9826284eb8735c8ce395448c93a2035
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-07 09:51:07 +0800

    Update Afrobeat index: add "Metallica - Damage IncDr" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Afrobeat.lua
+++ Uploaded Drums/returnfiles/songfiles/Afrobeat.lua
@@ -1,5 +1,6 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"JOJOs - Roundabout-Drum Kit, Drum Kit",
+	"Metallica - Damage IncDr",
 	"Thragg - Dont Fear The Reaper Drum Kit",
 }
