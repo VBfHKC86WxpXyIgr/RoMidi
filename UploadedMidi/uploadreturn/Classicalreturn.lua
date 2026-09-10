6b1dc3a198280f72da966d249298a2a81d9b5171
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-11 00:06:00 +0800

    Update Classical index: add "German Military March - Erika.Mid 1684675071981.Mid" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -122,6 +122,7 @@ return {
 	"G.F. Handel - Salmon",
 	"Galaxy Collapse (With Correct BPM)",
 	"GAYNOR.I Will Survive K",
+	"German Military March - Erika.Mid 1684675071981.Mid",
 	"Girls-Und-Panzer-Ost---Piano-On-Musescore",
 	"Gloria Gaynor Arr.  Elaine Kelkel - I Will Survive",
 	"Goo Goo Dolls - Iris [Official Music Video] [4K Remaster] 1764384558236",
