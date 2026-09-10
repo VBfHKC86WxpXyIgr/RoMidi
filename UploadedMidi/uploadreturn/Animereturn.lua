d8f5abf40ae2618d6be4442d0a9c8f4a3063f0b3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-11 00:00:11 +0800

    Update Anime index: add "Kyoko Kirigiri - Thick Of HiT Thick Of It X Thomas The Tank Engine" [piano] via uploader

--- UploadedMidi/uploadreturn/Animereturn.lua
+++ UploadedMidi/uploadreturn/Animereturn.lua
@@ -101,6 +101,7 @@ return {
 	"KISS OF DEATH（Produced By HYDE）(延音)",
 	"Kobayashi San Chi No Maid Dragon ED - Ishukan Communication (TV Size) - ShinkoNetCavy",
 	"Kokoronashi",
+	"Kyoko Kirigiri - Thick Of HiT Thick Of It X Thomas The Tank Engine",
 	"Light'S Theme - Death Note",
 	"Maid In Dragon",
 	"Manazashi Wa Hikari Tatsuya Kitani The Fragrant Flower Blooms With Dignity OP 1784992924078",
