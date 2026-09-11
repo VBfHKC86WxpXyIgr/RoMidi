65405da037d90386ff4cb0a2e5b8890ef3dd88cb
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-12 04:46:20 +0800

    Update Emotional index: add "Bedrott" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -60,6 +60,7 @@ return {
 	"Beabadoobee - Glue Song (Ft. Clairo)",
 	"Beatles (The) - Eleanor Rigby (2) [MIDIfind.Com]",
 	"Beatles (The) - While My Guitar Gently Weeps (2) [MIDIfind.Com]",
+	"Bedrott",
 	"Blooming-Afternoon-Mitsukiy(HUMAN BEST VERSION HARD)",
 	"Blue Jeans",
 	"Bunda By Melly Goeslaw - Fikram",
