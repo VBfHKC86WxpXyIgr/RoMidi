aba9ef02be82c69e72cd98137d2ba5d426545cb8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-11 13:18:49 +0800

    Update Memes index: add "Gdzie Jest Bialy Wegorz (Zejscie)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Memes.lua
+++ Uploaded Guitars/returnfiles/songfiles/Memes.lua
@@ -2,6 +2,7 @@
 return {
 	"1Bakamitai",
 	"1Buena Vida Mala Fama (King Nasir) - Crish Ramirez Playable",
+	"Gdzie Jest Bialy Wegorz (Zejscie)",
 	"Hammer Smashed Face",
 	"Home - Were Finally Landing 65702210",
 	"Michael Jackson - Billie Jean",
