5b65b8aa7f817a0065ba66b88323cd52a6537c7f
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-13 07:15:06 +0800

    Update Emotional index: add "Wonder If You Care" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -429,6 +429,7 @@ return {
 	"Where's My Love – SYML",
 	"Wifi Skeleton - Nope Your Too Late I Already Died (Midi)",
 	"Wiz Khalifa - See You Again ft Charlie Puth Lyrics",
+	"Wonder If You Care",
 	"XXXTENTACION - SAD!",
 	"XXXTentacion - Sad!",
 	"Yiruma - Maybe - Fikram",
