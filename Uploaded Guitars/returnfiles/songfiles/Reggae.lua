e8181df3a0f2e5fdb778781482a38a443246bbdd
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-14 01:55:40 +0800

    Update Reggae index: add "Misc Covers-Three Little Birds By Bob Marley (3)-09-13-2026" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Reggae.lua
+++ Uploaded Guitars/returnfiles/songfiles/Reggae.lua
@@ -1,6 +1,7 @@
 -- Auto-updated by the Cloudflare Worker on upload/delete
 return {
 	"Marley Bob - Jammin [MIDIfind.Com]",
+	"Misc Covers-Three Little Birds By Bob Marley (3)-09-13-2026",
 	"Sublime - Caress Me Down [MIDIfind.Com]",
 	"Sublime - Santeria",
 }
