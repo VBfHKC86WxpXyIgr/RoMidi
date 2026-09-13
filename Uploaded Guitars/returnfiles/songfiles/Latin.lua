6cc4e28c5e7b9f44fc1599b1d6b0cd69be7ba9c4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-14 01:55:14 +0800

    Update Latin index: add "Misc Covers-Kwwka-09-13-2026(1)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Latin.lua
+++ Uploaded Guitars/returnfiles/songfiles/Latin.lua
@@ -5,6 +5,7 @@ return {
 	"Desvelado By Bobby Pulido",
 	"La Resaka By Supermerk2",
 	"Misc Covers-Beretta By El De Las R'S.-07-04-2026",
+	"Misc Covers-Kwwka-09-13-2026(1)",
 	"Misc Covers-La Cheyenne By El De Las R’S-07-04-2026",
 	"Misc Covers-Negro Y Azul (The Heisenberg Song) By Breaking Bad-09-13-2026",
 	"Misc Covers-Nsjkks-09-13-2026",
