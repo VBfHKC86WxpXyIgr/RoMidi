925d04f937ea743d3b885d4de18f838986374c37
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-18 06:40:08 +0800

    Update Others index: add "F・L・Y - スペクトラム (Spectrum)" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -279,6 +279,7 @@ return {
 	"Evil Morty Meme Theme",
 	"Evil Morty Meme Theme (1)",
 	"F-L-Y - スペクトラム] (FLY By Spectrum)",
+	"F・L・Y - スペクトラム (Spectrum)",
 	"Fade To Black Piano Ig",
 	"Fake Love X Flower Dance",
 	"Fake Love X Suzume X BóNg Lá RơI",
