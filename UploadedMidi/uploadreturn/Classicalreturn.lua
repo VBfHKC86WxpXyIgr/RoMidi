784faa9c5ebdad35028182db2af18938198ab2b3
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-17 22:52:38 +0800

    Update Classical index: add "ピアノ楽譜Montagem-Hikari--Bellyjayソロ中級朝の光の中で (1)" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -335,6 +335,7 @@ return {
 	"테일즈위버 OST - Reminiscence - Yeon H (192k) (1)",
 	"테일즈위버-Ost---Second-Run-최고난이도-피아노-편곡--Second-Run-Extreme-Piano-Cover",
 	"테일즈위버-Talesweaver-Ost--Second-Run-Piano-Cover-피아노-커버",
+	"ピアノ楽譜Montagem-Hikari--Bellyjayソロ中級朝の光の中で (1)",
 	"プロジェクト 34",
 	"钢琴曲Blue Bossa",
 	"魔理沙は大変なものを盗んでいきました",
