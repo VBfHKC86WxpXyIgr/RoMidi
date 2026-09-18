75e7234d1e09dc0f5d5c5c1af729b0791374a092
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-18 22:05:34 +0800

    Update Classical index: add "Kangen - Dewa 19 - Fikram Trimmed 0.00s-300" [piano] via uploader

--- UploadedMidi/uploadreturn/Classicalreturn.lua
+++ UploadedMidi/uploadreturn/Classicalreturn.lua
@@ -156,6 +156,7 @@ return {
 	"Julius-Fucik-Entry-Of-The-Gladiators-Piano-Version",
 	"Jurassic Park Trex - Rescue- Finale Ending Credits Suite",
 	"Just The Two Of Us (MS)",
+	"Kangen - Dewa 19 - Fikram Trimmed 0.00s-300",
 	"Kanye West - Runaway (EPIC) Dreaming Piano Cover( -NXgt5cAgS8 )",
 	"Kara Sevda - Anlatamam - Piyano( 6VpRprnc8os )",
 	"Katyusha",
