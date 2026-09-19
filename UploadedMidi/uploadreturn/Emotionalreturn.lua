a0b725c2fb7cc9a2d51b4087a4b9ffe452ef5df6
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-19 08:05:31 +0800

    Update Emotional index: add "Finitude Gabriel Albu(BEST VERSION)" [piano] via uploader

--- UploadedMidi/uploadreturn/Emotionalreturn.lua
+++ UploadedMidi/uploadreturn/Emotionalreturn.lua
@@ -127,6 +127,7 @@ return {
 	"Faded",
 	"FantaisieImpromptu (BEST VERSION)",
 	"Feeling Good (BEST VERSION)",
+	"Finitude Gabriel Albu(BEST VERSION)",
 	"Floating In Reverie Moayad Aljabal - Fikram",
 	"Floating-In-Reverie-Moayad-Aljabal(MEDIUM VERSION)",
 	"Flower-Dance(BEST VERSION)",
