38da2619db3a5fdb3b5d516fe935bea447f61075
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-23 17:52:45 +0800

    Update Rock index: add "My Chemical Romance - Demolition Lovers (Songparts.Com)" [drum] via uploader

--- Uploaded Drums/returnfiles/songfiles/Rock.lua
+++ Uploaded Drums/returnfiles/songfiles/Rock.lua
@@ -148,6 +148,7 @@ return {
 	"Megadeth - Peace Sells [MIDIfind.Com]",
 	"Metallica Enter Sandman",
 	"Muse - Hysteria [MIDIfind.Com]",
+	"My Chemical Romance - Demolition Lovers (Songparts.Com)",
 	"My Chemical Romance - Give Em Hell Kid [MIDIfind.Com]",
 	"My Chemical Romance - I'M Not Okay I Promise [MIDIfind.Com]",
 	"My Chemical Romance - Na Na Na [MIDIfind.Com]",
