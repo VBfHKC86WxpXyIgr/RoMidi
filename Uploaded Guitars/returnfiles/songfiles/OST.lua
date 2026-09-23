5a9f7414e35eef58f1cafc2de65198644cd13f38
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-24 04:12:34 +0800

    Update OST index: add "Fastest-Thing-Alive-Full-Version (2)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/OST.lua
+++ Uploaded Guitars/returnfiles/songfiles/OST.lua
@@ -9,6 +9,7 @@ return {
 	"Dire, Dire Docks (Super Mario 64)",
 	"Doom - E1M1 (GM MIDI)",
 	"Ella Baila Sola Eslabon Armado Y Peso Pluma",
+	"Fastest-Thing-Alive-Full-Version (2)",
 	"Ginyu Force Theme - [Faulconer Productions]",
 	"Gohan Fights Frieza [Clean Cut]",
 	"Gotenks Is Born - [Faulconer Productions]",
