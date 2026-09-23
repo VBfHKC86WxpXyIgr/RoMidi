5b3e0eabb206859a5e3cac9e78578d67c5c356b5
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-23 19:14:54 +0800

    Update Punk index: add "Golden Brown (Guitar) (2)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Punk.lua
+++ Uploaded Guitars/returnfiles/songfiles/Punk.lua
@@ -9,6 +9,7 @@ return {
 	"Breed - Nirvana (TABS And Vocals)",
 	"CUPSIZE - Ty Lyubish Tancevat 76775527",
 	"Frances Farmer Will Have Her Revenge On Seattle - Nirvana",
+	"Golden Brown (Guitar) (2)",
 	"Green Day - American Eulogy [MIDIfind.Com]",
 	"Green Day - American Idiot (2) [MIDIfind.Com]",
 	"Green Day - Are We The Waiting [MIDIfind.Com]",
