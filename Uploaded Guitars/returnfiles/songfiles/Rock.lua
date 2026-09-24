077fe92d9738512730ef2ddbc491b41525ce04c4
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-24 17:22:12 +0800

    Update Rock index: add "Radiohead - Airbag Jonny" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -170,6 +170,7 @@ return {
 	"Radiohead - 2+2=5 [MIDIfind.Com]",
 	"Radiohead - Airbag (2) [MIDIfind.Com]",
 	"Radiohead - Airbag (Songparts.Com)",
+	"Radiohead - Airbag Jonny",
 	"Radiohead - Everything In Its Right Place [MIDIfind.Com]",
 	"Radiohead - Fake Plastic Trees [MIDIfind.Com]",
 	"Radiohead - Just [MIDIfind.Com]",
