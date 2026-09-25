d72b5b76924494967f3d893fab27797d26b7c299
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-25 17:44:38 +0800

    Update Rock index: add "My Chemical Romance - Thank You For The Venom Frank Iero" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -112,6 +112,7 @@ return {
 	"My Chemical Romance - Thank You For The Venom [MIDIfind Com]-20260818092349",
 	"My Chemical Romance - Thank You For The Venom [MIDIfind.Com]",
 	"My Chemical Romance - Thank You For The Venom Bass",
+	"My Chemical Romance - Thank You For The Venom Frank Iero",
 	"My Chemical Romance - Thank You For The Venom Ray Toro",
 	"My Chemical Romance - This Is How I Disappear",
 	"My Chemical Romance - This Is How I Disappearr",
