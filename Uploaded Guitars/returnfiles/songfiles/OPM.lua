e4959072c511d7db8cf84fcb98d4d54a69b4cb26
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-25 08:58:09 +0800

    Update OPM index: add "Go-Shiina-The-Song-Of-Kamado-Tanjiro-Ft-Nami-Nakagawa-Anonymous-20191222165421-Nonstop2k.Com 2" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/OPM.lua
+++ Uploaded Guitars/returnfiles/songfiles/OPM.lua
@@ -2,6 +2,7 @@
 return {
 	"Aladdin-(Medley-Of-All-Songs)",
 	"BRITNEY SPEARS.Toxic",
+	"Go-Shiina-The-Song-Of-Kamado-Tanjiro-Ft-Nami-Nakagawa-Anonymous-20191222165421-Nonstop2k.Com 2",
 	"Michael Jackson - Beat It",
 	"Never-Gonna-Give-You-Up-3",
 	"Titanic-3",
