323a3586c45fcea5ea07b06918a22fb1328e2e02
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-25 17:44:11 +0800

    Update Rock index: add "My Chemical Romance - Thank You For The Venom Ray Toro" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -112,6 +112,7 @@ return {
 	"My Chemical Romance - Thank You For The Venom [MIDIfind Com]-20260818092349",
 	"My Chemical Romance - Thank You For The Venom [MIDIfind.Com]",
 	"My Chemical Romance - Thank You For The Venom Bass",
+	"My Chemical Romance - Thank You For The Venom Ray Toro",
 	"My Chemical Romance - This Is How I Disappear",
 	"My Chemical Romance - This Is How I Disappearr",
 	"My Chemical Romance - You Know What They Do To Guys Like Us In Prison (2) [MIDIfind.Com]",
