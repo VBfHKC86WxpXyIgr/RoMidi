f1c443ca47c0f145a4cb6bbae7d0971735156b4b
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-26 06:47:12 +0800

    Update Others index: add "The Shop From Pikuniku" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -783,6 +783,7 @@ return {
 	"The Last Of Us - Gustavo Santaolalla (Piano Solo)",
 	"The Odyssey - Odysseus (EPIC Piano Cover)",
 	"The Perfect Girl",
+	"The Shop From Pikuniku",
 	"The-Entertainer-Jazz-Arrangement",
 	"The-Ocean-Waves-Ost-I-Can-Hear-The-Sea-Shigeru-Nagata-Studio-Ghiblis-The-Ocean-Waves",
 	"There-S-No-One-At-All",
