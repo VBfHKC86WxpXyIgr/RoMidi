0ffd2585c6d025181f1560c0c6b902b89b9d5c75
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-26 03:57:35 +0800

    Update Others index: add "Freedom Dive" [piano] via uploader

--- UploadedMidi/uploadreturn/Othersreturn.lua
+++ UploadedMidi/uploadreturn/Othersreturn.lua
@@ -329,6 +329,7 @@ return {
 	"Frank Sinatra - Fly Me To The Moon",
 	"Frank Sinatra - My Way But Improvising In Chopin Style - (Arr. Birru) (Live Midi) (1)",
 	"FREAKS",
+	"Freedom Dive",
 	"Freedom Dive - Xi (Japan) Freedom Dive (Impossible Version) (1)",
 	"Friqtao Huye",
 	"Galaxy Collapse - Kurokotei",
