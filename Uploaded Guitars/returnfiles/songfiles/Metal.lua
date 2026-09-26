f38534e201a55ddec90ef94592066ac0b35cffc1
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-26 15:11:06 +0800

    Update Metal index: add "Metallica - Ride The Lightning James Hetfield" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Metal.lua
+++ Uploaded Guitars/returnfiles/songfiles/Metal.lua
@@ -250,6 +250,7 @@ return {
 	"Metallica - Orion [MIDIfind.Com]",
 	"Metallica - Phantom Lord [MIDIfind.Com]",
 	"Metallica - Ride The Lightning",
+	"Metallica - Ride The Lightning James Hetfield",
 	"Metallica - Ride The Lightning Solo Tutorial By Chris Zoupa [MIDIfind.Com]",
 	"Metallica - Sad But True [MIDIfind.Com]",
 	"Metallica - Seek And Destroy",
