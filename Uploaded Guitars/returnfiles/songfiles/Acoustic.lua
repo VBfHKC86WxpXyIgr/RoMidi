5f08b411d341fd79568ad00ee0f9d7dd62a76010
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-26 08:33:41 +0800

    Update Acoustic index: add "Kroi - JOJO SPIN Greed Guitar1" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
+++ Uploaded Guitars/returnfiles/songfiles/Acoustic.lua
@@ -151,6 +151,7 @@ return {
 	"Kidhord Bodyslam Greed Guitar2 26",
 	"King Crimson - 21st Century Schizoid ManGreed",
 	"Kroi - JOJO SPIN Greed Bass",
+	"Kroi - JOJO SPIN Greed Guitar1",
 	"LArcenCiel - Drivers High",
 	"Led Zeppelin - Stairway To Heaven - Fingerstyle",
 	"Linked Horizon - Attack On Titan OpShingeki No Kyojin Op",
