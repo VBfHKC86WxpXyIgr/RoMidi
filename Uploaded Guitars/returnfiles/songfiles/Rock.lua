b3de7b2c6969818be5c70bb753a444bf3acee4df
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-26 19:33:21 +0800

    Update Rock index: add "Nirvana - Polly Guitar" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -142,6 +142,7 @@ return {
 	"Nirvana - Nirvana Lithium Live MTV Vma 1992 [MIDIfind.Com]",
 	"Nirvana - On A Plain (2) [MIDIfind.Com]",
 	"Nirvana - Opinion [MIDIfind.Com]",
+	"Nirvana - Polly Guitar",
 	"Nirvana - Rape Me [MIDIfind.Com]",
 	"Nirvana - Sappy",
 	"Nirvana - Scentless Apprentice (3) [MIDIfind.Com]",
