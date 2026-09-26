33327efdda3556bace7aeca793e657272c5875b8
VBfHKC86WxpXyIgr <120575042+VBfHKC86WxpXyIgr@users.noreply.github.com>
2026-09-26 19:11:53 +0800

    Update Rock index: add "Pierce The Veil - Bulls In The Bronx (Songparts.Com)" [guitar] via uploader

--- Uploaded Guitars/returnfiles/songfiles/Rock.lua
+++ Uploaded Guitars/returnfiles/songfiles/Rock.lua
@@ -171,6 +171,7 @@ return {
 	"Pearl Jam - Even Flow (3) [MIDIfind.Com]",
 	"Pearl Jam - Even Flow [MIDIfind.Com]",
 	"PEARL JAM.Even Flow",
+	"Pierce The Veil - Bulls In The Bronx (Songparts.Com)",
 	"Pierce The Veil - Bulls In The Bronx Bass",
 	"Pierce The Veil - Bulls In The Bronx Lead",
 	"Pierce The Veil - Bulls In The Bronx Rhythm",
